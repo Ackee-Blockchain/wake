39a67faa974f708af01de41ed3962c679ef93f21
hacker-DOM <dteiml@gmail.com>
2022-01-12 19:02:40 +0100

    :wrench: Add /woke/setup.py

---
+++ woke/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+install_requires = []
+
+tests_require = ['pytest >= 6.25, < 0.7']
+
+extras_require = dict(dev = [])
+
+setup(
+    name="woke",
+    description="Woke is a static analyzer and symbolic execution engine for Solidity.",
+    url="https://github.com/Ackee-Blockchain/woke",
+    author="Ackee Blockchain",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require=extras_require,
+    license="ISC",
+    long_description=open("README.adoc", "r", encoding="utf-8").read(),
+    entry_points=dict(console_scripts = [])
+)
