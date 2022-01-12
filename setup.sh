b117ee08049b67829cec25d1abf5832987bd5f7d
hacker-DOM <dteiml@gmail.com>
2022-01-12 19:08:36 +0100

    :wrench: Add /setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# To be run after `git clone`
+# INV: Should be idempotent
+
+# register .githooks as directory for git hooks path
+git config --local core.hooksPath .githooks/ # since git 2.9
+# make hooks executable
+chmod +x ./.githooks/*
+
+for i in "woke" "woke-js" "editors/code";
+do
+    chmod +x ./"$i"/.githooks/*
+done
