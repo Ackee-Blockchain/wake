bcad74b9896de2021d62b95c39b9a66d5415b736
hacker-DOM <dteiml@gmail.com>
2022-01-26 21:31:50 +0100

    :heavy_plus_sign: Add `portray` for static documentation generation

--- woke/setup.py
+++ woke/setup.py
@@ -5,7 +5,7 @@ install_requires = []
 # Also: [pyright](https://github.com/microsoft/pyright/) (distributed through npm)
 extras_require = dict(
     tests=["pytest >= 6.2.5, < 7.0"],
-    dev=["black"],
+    dev=["black, portray"],
 )
 
 setup(
