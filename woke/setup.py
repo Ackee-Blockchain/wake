9a6c3ea760df021f7e88d19c546ed09779e864c7
hacker-DOM <dteiml@gmail.com>
2022-01-26 19:39:25 +0100

    ::heavy_plus_sign: Add black, pylint, pyright

---
+++ woke/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+install_requires = []
+
+tests_require = ["pytest >= 6.25, < 0.7"]
+
+# Also: [pyright](https://github.com/microsoft/pyright/) (distributed through npm)
+extras_require = dict(dev=["black", "pylint"])
+
+setup(
+    name="woke",
+    description="Woke is a static analyzer and symbolic execution engine for Solidity.",
+    url="https://github.com/Ackee-Blockchain/woke",
+    author="Ackee Blockchain",
+    version="0.1.0",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require=extras_require,
+    license="ISC",
+    long_description=open("README.adoc", "r", encoding="utf-8").read(),
+    entry_points=dict(console_scripts=[]),
+)
