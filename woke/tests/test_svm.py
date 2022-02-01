fa64ba32edd0e995fc550dd763c7f3281c9625c2
Michal Převrátil <michprev@gmail.com>
2022-02-01 22:43:21 +0100

    :sparkles: svm: implement list_all and list_installed

--- woke/tests/test_svm.py
+++ woke/tests/test_svm.py
@@ -25,8 +25,13 @@ def run_cleanup(request):
 @pytest.mark.parametrize("run_cleanup", [[PYTEST_WOKE_PATH]], indirect=True)
 def test_basic_usage(run_cleanup):
     svm = SolcVersionManager(woke_root_path=PYTEST_WOKE_PATH)
+
+    assert len(svm.list_installed()) == 0
+    assert "0.8.10" in svm.list_all()
     svm.install("0.8.10")
+    assert "0.8.10" in svm.list_installed()
     svm.remove("0.8.10")
+    assert len(svm.list_installed()) == 0
 
 
 @pytest.mark.parametrize("run_cleanup", [[PYTEST_WOKE_PATH]], indirect=True)
@@ -34,6 +39,7 @@ def test_install_invalid_version(run_cleanup):
     svm = SolcVersionManager(woke_root_path=PYTEST_WOKE_PATH)
     with pytest.raises(ValueError):
         svm.install("0.8.a")
+    assert len(svm.list_installed()) == 0
 
 
 @pytest.mark.parametrize(
@@ -42,8 +48,17 @@ def test_install_invalid_version(run_cleanup):
 def test_two_woke_root_paths(run_cleanup):
     svm1 = SolcVersionManager(woke_root_path=PYTEST_WOKE_PATH)
     svm2 = SolcVersionManager(woke_root_path=str(PYTEST_WOKE_PATH2))
+
+    assert "0.8.10" in svm1.list_all()
+    assert "0.8.9" in svm2.list_all()
+    assert len(svm1.list_installed()) == 0
+    assert len(svm2.list_installed()) == 0
     svm1.install("0.8.10")
     svm2.install("0.8.9")
+    assert "0.8.10" in svm1.list_installed()
+    assert "0.8.9" not in svm1.list_installed()
+    assert "0.8.9" in svm2.list_installed()
+    assert "0.8.10" not in svm2.list_installed()
 
 
 @pytest.mark.parametrize("run_cleanup", [[PYTEST_WOKE_PATH]], indirect=True)
