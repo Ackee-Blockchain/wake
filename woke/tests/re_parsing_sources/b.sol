cb80207cd0ff7a626a330eadf1891ca610118cb1
Michal Převrátil <michprev@gmail.com>
2022-02-11 13:26:02 +0100

    :white_check_mark: Add tests for SoliditySourceParser

---
+++ woke/tests/re_parsing_sources/b.sol
@@ -0,0 +1,9 @@
+pragma solidity 0
+.8
+.11 -0.9.2;
+
+import*as xyz from "a.sol";
+import "a.sol";
+
+contract b {
+}
---
+++ woke/tests/re_parsing_sources/d.sol
@@ -0,0 +1,19 @@
+import 'abc_2$@#$\'ax\"';
+import "\'__  123\"";
+import
+*as xyz
+from
+'a.sol';
+
+// import "xyz";
+
+/*
+----------
+import "ijk";
+----------
+*/
+
+contract d {
+    string a = "import '123';";
+    string tmp = 'random string here'; string x = 'import "456";';
+}
