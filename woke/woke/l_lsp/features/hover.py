f09bafcc6677509b350aed07600eb544cf445382
bemic <bemic.ml@gmail.com>
2022-02-26 14:46:31 +0100

    ✨ LSP feature - hover

---
+++ woke/woke/l_lsp/features/hover.py
@@ -0,0 +1,34 @@
+from typing import List, Optional, Union
+from server.structures import (
+                                MarkupContent, 
+                                MarkupKind,
+                                Range,
+                                TextDocumentPositionParams, 
+                                WorkDoneProgressOptions,
+                                WorkDoneProgressParams)
+
+
+class HoverClientCapabilities:
+    dynamic_registration: Optional[bool]
+    content_format: Optional[List[MarkupKind]]
+
+
+class HoverOptions(WorkDoneProgressOptions):
+    pass
+
+
+class HoverParams(TextDocumentPositionParams, WorkDoneProgressParams):
+    pass
+
+
+class MarkedString:
+    language: str
+    value: str
+
+
+MarkedStringType = Union[str, MarkedString]
+
+
+class Hover:
+    contents: Union[MarkedStringType, List[MarkedStringType], MarkupContent]
+    range: Optional[Range]
