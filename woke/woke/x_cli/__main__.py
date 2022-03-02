a1ea55c50b05a39899c72f54bd89f5f784e6f923
Michal Převrátil <michprev@gmail.com>
2022-03-02 14:35:54 +0100

    :sparkles: Implement `woke config` subcommand

--- woke/woke/x_cli/__main__.py
+++ woke/woke/x_cli/__main__.py
@@ -2,6 +2,9 @@ from click.core import Context
 import click
 import rich.traceback
 
+from woke.a_config import WokeConfig
+from .console import console
+
 
 @click.group()
 @click.option("--debug/--no-debug", default=True)
@@ -10,4 +13,13 @@ def main(ctx: Context, debug: bool) -> None:
     ctx.ensure_object(dict)
     ctx.obj["debug"] = debug
 
-    rich.traceback.install(show_locals=True, suppress=[click])
+    rich.traceback.install(show_locals=True, suppress=[click], console=console)
+
+
+@main.command(name="config")
+@click.pass_context
+def config(ctx: Context) -> None:
+    """Print loaded config options in JSON format."""
+    config = WokeConfig()
+    config.load_configs()
+    console.print_json(str(config))
