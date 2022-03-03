e9d254222bf5378777d18c4fb5cb72303311f386
Michal Převrátil <michprev@gmail.com>
2022-03-03 17:47:24 +0100

    :bug: Fix `include_paths` config option for solc <0.8.8

--- woke/woke/d_compile/solc_frontend/solc_runner.py
+++ woke/woke/d_compile/solc_frontend/solc_runner.py
@@ -6,7 +6,12 @@ import asyncio
 from woke.a_config import WokeConfig
 from woke.b_svm import SolcVersionManager
 from woke.c_regex_parsing import SolidityVersion
-from .input_data_model import SolcInput, SolcInputSource, SolcInputSettings
+from .input_data_model import (
+    SolcInput,
+    SolcInputSource,
+    SolcInputSettings,
+    SolcInputLanguageEnum,
+)
 from .output_data_model import SolcOutput
 from .exceptions import CompilationError
 
@@ -29,10 +34,19 @@ class SolcFrontend:
     async def compile_files(
         self, files: Dict[str, Path], target_version: SolidityVersion
     ) -> SolcOutput:
-        standard_input = SolcInput()
+        standard_input = SolcInput()  # type: ignore
 
         for unit_name, path in files.items():
-            standard_input.sources[unit_name] = SolcInputSource(urls=[str(path)])
+            if target_version >= "0.8.8":
+                # path = {include_path} / {unit_name}
+                # since 0.8.8 include paths can be passed as cmdline arguments to solc
+                # because of this, source unit names can be passed here instead of (full) absolute paths
+                standard_input.sources[unit_name] = SolcInputSource(urls=[unit_name])
+            else:
+                # for solc versions < 0.8.8 include paths cannot be passed as cmdline arguments to solc
+                # because of this, absolute paths must be used here
+                standard_input.sources[unit_name] = SolcInputSource(urls=[str(path)])
+
         standard_input.settings = SolcInputSettings()  # type: ignore
         standard_input.settings.evm_version = self.__config.compiler.solc.evm_version
         standard_input.settings.remappings = [
@@ -54,8 +68,9 @@ class SolcFrontend:
         )
         args.append(f"--allow-paths=.,{allow_paths}")
 
-        for include_path in self.__config.compiler.solc.include_paths:
-            args.append(f"--include-path={include_path}")
+        if target_version >= "0.8.8":
+            for include_path in self.__config.compiler.solc.include_paths:
+                args.append(f"--include-path={include_path}")
 
         if target_version >= "0.6.9":
             args.append("--base-path=.")
