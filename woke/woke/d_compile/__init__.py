67d0d4633a9205f556bf4fe7c337b0488cf8b0cf
Michal Převrátil <michprev@gmail.com>
2022-04-02 15:32:55 +0200

    :construction: Return `SolcOutput` from compile submodule

--- woke/woke/d_compile/__init__.py
+++ woke/woke/d_compile/__init__.py
@@ -1,1 +1,3 @@
 from .compiler import SolidityCompiler
+from .solc_frontend.input_data_model import SolcOutputSelectionEnum
+from .solc_frontend.output_data_model import SolcOutput, SolcOutputContractInfo
