b1fec592a055e399319e8b8aa4630b1587152f00
bemic <bemic.ml@gmail.com>
2022-04-20 20:55:54 +0200

    ✨ Context for for argument passing

---
+++ woke/woke/l_lsp/context.py
@@ -0,0 +1,34 @@
+from typing import List, Optional, Union
+
+from woke.a_config import WokeConfig
+from .enums import TraceValueEnum
+from basic_structures import *
+
+
+class LSPContext:
+    woke_config: Optional[WokeConfig]
+    shutdown_received: bool
+    initialized: bool
+    trace_value: TraceValueEnum
+    client_capabilities: dict
+    #workspace: Union[dict, None]
+
+
+    def __init__(self) -> None:
+        self.woke_config = None
+        self.shutdown_received = False
+        self.initialized = False
+        self.trace_value = TraceValueEnum.OFF
+    '''
+    def update_workspace(self,
+                        document_uri: DocumentUri,
+                        document_veriosn: int, 
+                        document_change: TextDocumentContentChangeEvent):
+        old_doc = self.workspace[document_uri]
+        if document_change.range is None:
+            self.workspace[document_uri].text = document_change.text
+        else:
+            change_from = document_change.range.start
+            change_to = document_change.range.end
+            self.workspace[document_uri].text[]
+    '''
