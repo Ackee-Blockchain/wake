320d964cd74367272664b5611bc8bb34cbba75c6
bemic <bemic.ml@gmail.com>
2022-04-20 20:52:14 +0200

    🏗️ remove client capabilities

--- woke/woke/l_lsp/server.py
+++ woke/woke/l_lsp/server.py
@@ -1,21 +1,22 @@
+from typing import Union
 import logging
+
 from context import LSPContext
 from protocol_structures import NotificationMessage, RequestMessage, ResponseMessage, ResponseError, ErrorCodes
 from RPC_protocol import RPCProtocol
 from methods import RequestMethodEnum
 from methods_impl import method_mapping
 from notifications_impl import notification_mapping
-from typing import Union
+
 
 
 
 class Server:
-    def __init__(self, protocol: RPCProtocol, client_capabilities: list, threads: int = 1):
+    def __init__(self, protocol: RPCProtocol, threads: int = 1):
         self.protocol = protocol
         self.threads = threads 
-        self.client_capabilities = client_capabilities
+        self.client_capabilities = {}
         self.running = True
-        self.workspace = None
         self.init_request_received = False
         self.context = LSPContext()
         
@@ -25,14 +26,13 @@ class Server:
         '''
         while self.running:
             try:
-                # Read it
+                # Read the message 
                 message = self.protocol.recieve_message()
-                print(f'MESSAGE:\n{message.dict()}\n-------------------\n')
                 if type(message) == RequestMessage:
-                    print('*REQUEST')
+                    #print('* REQUEST *')
                     self.handle_message(message)
                 else:
-                    print('*NOTIFICATION')
+                    #print('* NOTIFICATION  *')
                     self.handle_notification(message)             
             except EOFError:
                 break
@@ -46,39 +46,41 @@ class Server:
     def handle_message(self, request: RequestMessage):
         # Double initialization
         if self.init_request_received and request.method == RequestMethodEnum.INITIALIZE:
-            logging.error("Double init")
+            #logging.error("Double init")
             response_message = self._serve_error(request, ErrorCodes.InvalidRequest.value, "Server already initialized")
             self.protocol.send_rpc_response(response_message)
             return
         # Init before request needed  
         if request.method != RequestMethodEnum.INITIALIZE and not self.init_request_received:
-            logging.error("Request before init")
+            #logging.error("Request before init")
             response_message = self._serve_error(request, ErrorCodes.ServerNotInitialized.value, "Server has not been initialized")                                
             self.protocol.send_rpc_response(response_message)
             return           
-        # Handle here
+        # Handling request
         try:
-            logging.info(f"Handling\nRequest id: {request.id}\nRequest method: {request.method}")
+            #logging.info(f"Handling\nRequest id: {request.id}\nRequest method: {request.method}")
             response =  self._serve_response(request)
             self.protocol.send_rpc_response(response)
         except Exception as e:
-            logging.error(e)
+            #logging.error(e)
             error = self._serve_error(request, ErrorCodes.RequestCancelled, "Error in handling message")
             self.protocol.send_rpc_response(error)
 
     def handle_notification(self, notification: NotificationMessage):
-        print('handling')
+        #print('handling')
+        # Handling notification
+        # No error response send after failed notification
         try:
             self._serve_notification(notification)
-            print('h')
         except Exception as e:
             logging.error(e)
 
     def _serve_response(self, request: RequestMessage) ->  ResponseMessage:
-        print('serving')
+        #print('serving')
         response = method_mapping[request.method](self.context, request.params)
         if self.context.initialized:
             self.init_request_received = True
+            self.client_capabilities = self.context.client_capabilities
         if self.context.shutdown_received:
             self.stop_server()
         response_message = ResponseMessage(
@@ -100,7 +102,8 @@ class Server:
         return response_message
 
     def _serve_notification(self, request: RequestMessage) -> None:
-        # Methods do their job
+        # Server handles notification
+        # Nothing to return
         notification_mapping[request.method](self.context, request.params)
         return
 
