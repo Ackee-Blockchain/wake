fabc08f4c3110d352967bbd03b81c7c0c3f073f1
Michal Převrátil <michprev@gmail.com>
2022-04-22 09:27:23 +0200

    :white_check_mark: Add tests for `woke svm` subcommands

---
+++ woke/tests/test_svm_cli.py
@@ -0,0 +1,166 @@
+import re
+import shutil
+from pathlib import Path
+
+import pytest
+from click.testing import CliRunner
+
+from woke.a_config import WokeConfig
+from woke.b_svm import SolcVersionManager
+from woke.c_regex_parsing import SolidityVersion
+from woke.x_cli.__main__ import main
+
+
+PYTEST_WOKE_ROOT_PATH = (Path.home() / ".tmpwoke_Z6yVySfqSk").resolve()
+
+
+@pytest.fixture()
+def woke_root_path():
+    PYTEST_WOKE_ROOT_PATH.mkdir()
+    yield
+    shutil.rmtree(PYTEST_WOKE_ROOT_PATH, True)
+
+
+@pytest.mark.slow
+def test_svm_install(woke_root_path):
+    config = WokeConfig(woke_root_path=PYTEST_WOKE_ROOT_PATH)
+    config.load_configs()
+    svm = SolcVersionManager(config)
+    cli_runner = CliRunner()
+
+    latest_version = next(reversed(svm.list_all()))
+    cli_result = cli_runner.invoke(
+        main, [f"--woke-root-path={PYTEST_WOKE_ROOT_PATH}", "svm", "install", "*"]
+    )
+    print(cli_result.output)
+    assert cli_result.exit_code == 0
+    assert latest_version in svm.list_installed()
+
+
+@pytest.mark.slow
+def test_svm_switch(woke_root_path):
+    config = WokeConfig(woke_root_path=PYTEST_WOKE_ROOT_PATH)
+    config.load_configs()
+    svm = SolcVersionManager(config)
+    cli_runner = CliRunner()
+
+    cli_result = cli_runner.invoke(
+        main, [f"--woke-root-path={PYTEST_WOKE_ROOT_PATH}", "svm", "switch", "0.6.2"]
+    )
+    assert cli_result.exit_code != 0
+    cli_result = cli_runner.invoke(
+        main, [f"--woke-root-path={PYTEST_WOKE_ROOT_PATH}", "svm", "install", "0.6.2"]
+    )
+    assert cli_result.exit_code == 0
+    assert len(svm.list_installed()) == 1
+    cli_result = cli_runner.invoke(
+        main, [f"--woke-root-path={PYTEST_WOKE_ROOT_PATH}", "svm", "switch", "0.6.2"]
+    )
+    assert cli_result.exit_code == 0
+
+    version_str = (PYTEST_WOKE_ROOT_PATH / ".woke_solc_version").read_text()
+    version = SolidityVersion.fromstring(version_str)
+    assert version in svm.list_installed()
+    assert version == "0.6.2"
+
+
+@pytest.mark.slow
+def test_svm_use(woke_root_path):
+    config = WokeConfig(woke_root_path=PYTEST_WOKE_ROOT_PATH)
+    config.load_configs()
+    svm = SolcVersionManager(config)
+    cli_runner = CliRunner()
+
+    cli_result = cli_runner.invoke(
+        main, [f"--woke-root-path={PYTEST_WOKE_ROOT_PATH}", "svm", "use", "0.8.4"]
+    )
+    assert cli_result.exit_code == 0
+    assert len(svm.list_installed()) == 1
+
+    version_str = (PYTEST_WOKE_ROOT_PATH / ".woke_solc_version").read_text()
+    version = SolidityVersion.fromstring(version_str)
+    assert version in svm.list_installed()
+    assert version == "0.8.4"
+
+
+@pytest.mark.slow
+def test_svm_list(woke_root_path):
+    config = WokeConfig(woke_root_path=PYTEST_WOKE_ROOT_PATH)
+    config.load_configs()
+    cli_runner = CliRunner()
+
+    cli_result = cli_runner.invoke(
+        main, [f"--woke-root-path={PYTEST_WOKE_ROOT_PATH}", "svm", "install", "0.7.6"]
+    )
+    assert cli_result.exit_code == 0
+    cli_result = cli_runner.invoke(
+        main, [f"--woke-root-path={PYTEST_WOKE_ROOT_PATH}", "svm", "install", "0.8.0"]
+    )
+    assert cli_result.exit_code == 0
+    cli_result = cli_runner.invoke(
+        main, [f"--woke-root-path={PYTEST_WOKE_ROOT_PATH}", "svm", "list"]
+    )
+    assert cli_result.exit_code == 0
+
+    assert re.search(r"^.*\s0\.7\.6\s*$", cli_result.output, re.MULTILINE)
+    assert re.search(r"^.*\s0\.8\.0\s*$", cli_result.output, re.MULTILINE)
+
+
+@pytest.mark.slow
+def test_svm_list_all(woke_root_path):
+    config = WokeConfig(woke_root_path=PYTEST_WOKE_ROOT_PATH)
+    config.load_configs()
+    cli_runner = CliRunner()
+
+    cli_result = cli_runner.invoke(
+        main, [f"--woke-root-path={PYTEST_WOKE_ROOT_PATH}", "svm", "install", "0.4.20"]
+    )
+    assert cli_result.exit_code == 0
+    cli_result = cli_runner.invoke(
+        main, [f"--woke-root-path={PYTEST_WOKE_ROOT_PATH}", "svm", "install", "0.6.3"]
+    )
+    assert cli_result.exit_code == 0
+    cli_result = cli_runner.invoke(
+        main, [f"--woke-root-path={PYTEST_WOKE_ROOT_PATH}", "svm", "list", "--all"]
+    )
+    assert cli_result.exit_code == 0
+
+    assert re.search(r"^.*\s0\.4\.19\s*$", cli_result.output, re.MULTILINE)
+    assert re.search(r"^.*\s0\.4\.20\s.*installed.*$", cli_result.output, re.MULTILINE)
+    assert re.search(r"^.*\s0\.5\.4\s*$", cli_result.output, re.MULTILINE)
+    assert re.search(r"^.*\s0\.6\.3\s.*installed.*$", cli_result.output, re.MULTILINE)
+    assert re.search(r"^.*\s0\.8\.12\s*$", cli_result.output, re.MULTILINE)
+
+
+@pytest.mark.slow
+def test_svm_remove(woke_root_path):
+    config = WokeConfig(woke_root_path=PYTEST_WOKE_ROOT_PATH)
+    config.load_configs()
+    svm = SolcVersionManager(config)
+    cli_runner = CliRunner()
+
+    cli_result = cli_runner.invoke(
+        main, [f"--woke-root-path={PYTEST_WOKE_ROOT_PATH}", "svm", "remove", "0.6.5"]
+    )
+    assert cli_result.exit_code != 0
+    cli_result = cli_runner.invoke(
+        main,
+        [
+            f"--woke-root-path={PYTEST_WOKE_ROOT_PATH}",
+            "svm",
+            "remove",
+            "0.6.5",
+            "--ignore-missing",
+        ],
+    )
+    assert cli_result.exit_code == 0
+    cli_result = cli_runner.invoke(
+        main, [f"--woke-root-path={PYTEST_WOKE_ROOT_PATH}", "svm", "install", "0.6.3"]
+    )
+    assert cli_result.exit_code == 0
+    assert "0.6.3" in svm.list_installed()
+    cli_result = cli_runner.invoke(
+        main, [f"--woke-root-path={PYTEST_WOKE_ROOT_PATH}", "svm", "remove", "0.6.3"]
+    )
+    assert cli_result.exit_code == 0
+    assert len(svm.list_installed()) == 0
