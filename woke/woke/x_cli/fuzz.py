84e2f0d250a8f916944316f03a9eecf4c246a9c6
Michal Převrátil <michprev@gmail.com>
2022-05-07 09:53:39 +0200

    :construction: Accept also dirs in `woke fuzz` command

--- woke/woke/x_cli/fuzz.py
+++ woke/woke/x_cli/fuzz.py
@@ -15,27 +15,39 @@ MODULE_NAME = "woke_fuzz_tests"
 
 
 @click.command(name="fuzz")
-@click.argument("files", nargs=-1, type=click.Path(exists=True))
+@click.argument("paths", nargs=-1, type=click.Path(exists=True))
 @click.option(
     "--process-count",
     default=(multiprocessing.cpu_count()),
     help="Number of processes to create for fuzzing.",
 )
 @click.pass_context
-def run_fuzz(ctx: click.Context, files: Tuple[str], process_count: int) -> None:
+def run_fuzz(ctx: click.Context, paths: Tuple[str], process_count: int) -> None:
     """Run Woke fuzzer."""
     config = WokeConfig(woke_root_path=ctx.obj["woke_root_path"])
     config.load_configs()  # load ~/.woke/config.toml and ./woke.toml
 
-    if len(files) == 0:
-        raise ValueError("No test files provided.")
+    if len(paths) == 0:
+        paths = (str(config.project_root_path / "test"),)
 
-    for file in files:
-        fuzz_file = Path(file)
-        if not fuzz_file.is_file() or not fuzz_file.match("*.py"):
-            raise ValueError(f"'{fuzz_file}' is not a Python file.")
+    py_files = []
 
-        module_spec = importlib.util.spec_from_file_location(MODULE_NAME, fuzz_file)
+    for path in paths:
+        fuzz_path = Path(path)
+
+        if fuzz_path.is_file() and fuzz_path.match("*.py"):
+            py_files.append(fuzz_path)
+        elif fuzz_path.is_dir():
+            for p in fuzz_path.rglob("*.py"):
+                if p.is_file():
+                    py_files.append(p)
+        else:
+            raise ValueError(f"'{fuzz_path}' is not a Python file or directory.")
+
+    fuzz_functions = []
+
+    for file in py_files:
+        module_spec = importlib.util.spec_from_file_location(MODULE_NAME, file)
         if module_spec is None or module_spec.loader is None:
             raise ValueError()
         module = importlib.util.module_from_spec(module_spec)
@@ -47,5 +59,8 @@ def run_fuzz(ctx: click.Context, files: Tuple[str], process_count: int) -> None:
             if func.__module__ == MODULE_NAME and func.__name__.startswith("test")
         )
         for func in functions:
-            console.print(f"Found '{func.__name__}' function in '{fuzz_file}' file.")
-            fuzz(config, func, process_count)
+            console.print(f"Found '{func.__name__}' function in '{file}' file.")
+            fuzz_functions.append(func)
+
+    for func in fuzz_functions:
+        fuzz(config, func, process_count)
