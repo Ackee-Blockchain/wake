be5ee62d7329efc6d7e6501d44307ff91d030ebe
Michal Převrátil <michprev@gmail.com>
2022-05-09 14:51:40 +0200

    :sparkles: Initial version of fuzz campaign, decorators, utils

---
+++ woke/woke/m_fuzz/decorators.py
@@ -0,0 +1,66 @@
+import inspect
+from typing import Callable, Optional, Tuple, TypeVar
+
+from .campaign import logger
+
+ModelUpdater = Callable[..., Optional[Tuple]]
+TxnReturn = TypeVar("TxnReturn")
+Txn = Callable[..., TxnReturn]
+TxnEnhanced = Callable[..., TxnReturn]
+Decorator = Callable[..., Txn]
+
+
+def model(model_updater: ModelUpdater) -> Decorator:
+    def decorator(txn_enhanced: TxnEnhanced) -> Txn:
+        def txn(*args, **kwars) -> TxnReturn:  # type: ignore
+            m_res = model_updater(*args)
+            if m_res is None:
+                return txn_enhanced(*args)  # type: ignore
+            else:
+                t_names = inspect.signature(txn_enhanced).parameters.keys()
+                m_names = inspect.signature(model_updater).parameters.keys()
+                # Sanity check
+                assert len(m_res) == len(t_names) - len(m_names)
+                for m, t in zip(m_names, t_names):
+                    assert m == t
+                for idx, p in enumerate(tuple(t_names)[len(m_names) :]):
+                    logger.debug(f"{p} = {m_res[idx]}")
+                return txn_enhanced(*args, *m_res)  # type: ignore
+
+        return txn  # type:ignore
+
+    return decorator
+
+
+def ignore(fn):
+    fn.ignore = True
+    return fn
+
+
+def rule(fn):
+    fn.rule = True
+    return fn
+
+
+def precondition(_precondition):
+    def decorator(fn):
+        fn.precondition = _precondition
+        return fn
+
+    return decorator
+
+
+def max_times(x: int):
+    def decorator(fn):
+        fn.max_times = x
+        return fn
+
+    return decorator
+
+
+def weight(w: int):
+    def decorator(fn):
+        fn.weight = w
+        return fn
+
+    return decorator
---
+++ woke/woke/m_fuzz/utils.py
@@ -0,0 +1,60 @@
+from collections import defaultdict
+import math
+import inspect
+from typing import Callable, Iterable, List, Tuple, TypeVar
+
+from Cryptodome.Hash import keccak
+
+
+def format_int(x: int) -> str:
+    if abs(x) < 10**5:
+        return f"{x:_}"
+    no_of_digits = int(math.log10(abs(x))) + 1
+    if x % 10 ** (no_of_digits - 3) == 0:
+        return f"{x:.2e}"
+    return f"{x:.2E} ({x:_})"
+
+
+def exists(a) -> bool:
+    if a is None:
+        return False
+    if len(a) >= 0:
+        return True
+    return bool(a)
+
+
+T = TypeVar("T")
+
+
+def partition(
+    seq: Iterable[T], predicate: Callable[[T], bool]
+) -> Tuple[List[T], List[T]]:
+    p = []
+    not_p = []
+    for el in seq:
+        if predicate(el):
+            p.append(el)
+        else:
+            not_p.append(el)
+    return p, not_p
+
+
+def keccak256(b: bytes) -> bytes:
+    h = keccak.new(data=b, digest_bits=256)
+    return h.digest()
+
+
+def get_current_fn_name(back_count=1) -> str:
+    frame = inspect.currentframe()
+    assert frame is not None
+    for _ in range(back_count):
+        frame = frame.f_back
+        assert frame is not None
+    return frame.f_code.co_name
+
+
+def negate(fn):
+    def inner(*args, **kwargs) -> bool:
+        return not fn(*args, **kwargs)
+
+    return inner
