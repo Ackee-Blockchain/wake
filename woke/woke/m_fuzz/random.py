f11e5c84f48fa79a0a697d6d403760d75d7fa149
Michal Převrátil <michprev@gmail.com>
2022-05-13 21:42:25 +0200

    :sparkles: Add fuzzer helpers for generating random objects

---
+++ woke/woke/m_fuzz/random.py
@@ -0,0 +1,85 @@
+import random
+import string
+from typing import Optional, Callable
+
+import brownie
+from brownie.network.account import Account
+
+
+def random_account(predicate: Optional[Callable[[Account], bool]] = None) -> Account:
+    if predicate is None:
+        acc = brownie.accounts
+    else:
+        acc = [account for account in brownie.accounts if predicate(account)]
+    return random.choice(acc)
+
+
+def random_int(min: int, max: int) -> int:
+    """like random.randint, but with the following probability distribution:
+    if min < 0 and max > 0:
+        20%: min
+        20%:   0
+        20%: max
+        40%: random.randint(min, max)
+    else:
+        20%: min
+        20%: max
+        60%: random.randint(min, max)"""
+    if min < 0 and max > 0:
+        p = random.random()
+        if p < 0.2:
+            res = min
+        elif p < 0.4:
+            res = 0
+        elif p < 0.6:
+            res = max
+        else:
+            res = random.randint(min, max)
+    else:
+        p = random.random()
+        if p < 0.2:
+            res = min
+        elif p < 0.4:
+            res = max
+        else:
+            res = random.randint(min, max)
+    return res
+
+
+def random_bool() -> bool:
+    return random.choice([True, False])
+
+
+def random_string(
+    min: int,
+    max: int,
+    alphabet: str = string.ascii_letters,
+    predicate: Optional[Callable[[str], bool]] = None,
+) -> str:
+    def gen() -> str:
+        len = random.randint(min, max)
+        return "".join(random.choice(alphabet) for _ in range(len))
+
+    ret = gen()
+    if predicate is None:
+        return ret
+
+    while not predicate(ret):
+        ret = gen()
+    return ret
+
+
+def random_bytes(
+    min: int, max: int, predicate: Optional[Callable[[bytes], bool]] = None
+) -> bytes:
+    def gen() -> bytes:
+        len = random.randint(min, max)
+        return bytes(random.getrandbits(8) for _ in range(len))
+
+    ret = gen()
+    if predicate is None:
+        return ret
+
+    while not predicate(ret):
+        ret = gen()
+    return ret
