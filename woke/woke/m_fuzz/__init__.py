75d4b3ca90417c1758f73f33872546410761b5fa
Michal Převrátil <michprev@gmail.com>
2022-06-06 20:38:25 +0200

    :art: Rename fuzzer rules to flows

--- woke/woke/m_fuzz/__init__.py
+++ woke/woke/m_fuzz/__init__.py
@@ -1,3 +1,3 @@
 from .campaign import Campaign, logger
-from .decorators import model, ignore, rule, invariant, precondition, max_times, weight
+from .decorators import model, ignore, flow, invariant, precondition, max_times, weight
 from .fuzzer import fuzz
