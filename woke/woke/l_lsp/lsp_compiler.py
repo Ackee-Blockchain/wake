545389d5bf6f5b4ff60ccecb67efe64f9efe7feb
Michal Převrátil <michprev@gmail.com>
2022-06-15 08:46:35 +0200

    :construction: Build IR in LSP compiler

--- woke/woke/l_lsp/lsp_compiler.py
+++ woke/woke/l_lsp/lsp_compiler.py
@@ -6,12 +6,16 @@ import sys
 import threading
 from pathlib import Path
 from threading import Thread
-from typing import Dict, List, Set, Optional, Union, Tuple
+from typing import Dict, List, Set, Union, Tuple, Collection, Mapping
 from urllib.parse import urlparse
 
 from woke.a_config import WokeConfig
 from woke.d_compile import SolcOutput, SolcOutputSelectionEnum
+from woke.d_compile.compilation_unit import CompilationUnit
 from woke.d_compile.compiler import SolidityCompiler
+from woke.d_compile.solc_frontend import SolcOutputError, SolcOutputErrorSeverityEnum
+from woke.e_ast_parsing.b_solc.c_ast_nodes import AstSolc
+from woke.e_ast_parsing.ir.source_unit import SourceUnit
 from woke.l_lsp.document_sync import (
     DidOpenTextDocumentParams,
     DidChangeTextDocumentParams,
@@ -50,9 +54,10 @@ class LspCompiler:
     __opened_files: Set[Path]
     __modified_files: Set[Path]
     __compiler: SolidityCompiler
-    __output: Optional[List[SolcOutput]]
     __output_contents: Dict[Path, str]
-
+    __errors: Set[SolcOutputError]
+    __asts: Dict[Path, AstSolc]
+    __source_units: Dict[Path, SourceUnit]
     __line_indexes: Dict[Path, List[Tuple[bytes, int]]]
 
     output_ready: threading.Event
@@ -64,7 +69,9 @@ class LspCompiler:
         self.__opened_files = set()
         self.__modified_files = set()
         self.__compiler = SolidityCompiler(config)
-        self.__output = None
+        self.__asts = {}
+        self.__errors = set()
+        self.__source_units = {}
         self.__line_indexes = {}
         self.__output_contents = dict()
 
@@ -75,8 +82,16 @@ class LspCompiler:
         self.__thread.start()
 
     @property
-    def output(self) -> Optional[List[SolcOutput]]:
-        return self.__output
+    def asts(self) -> Dict[Path, AstSolc]:
+        return self.__asts
+
+    @property
+    def errors(self) -> Set[SolcOutputError]:
+        return self.__errors
+
+    @property
+    def source_units(self) -> Dict[Path, SourceUnit]:
+        return self.__source_units
 
     @staticmethod
     def uri_to_path(uri: str) -> Path:
@@ -114,6 +129,40 @@ class LspCompiler:
         line_offset = byte_offset - prefix_sum
         return line_num, line_offset
 
+    def __compile(self, files: Collection[Path], modified_files: Mapping[Path, str]):
+        out: List[Tuple[CompilationUnit, SolcOutput]] = asyncio.run(
+            self.__compiler.compile(
+                files,
+                [SolcOutputSelectionEnum.AST],
+                write_artifacts=False,
+                reuse_latest_artifacts=False,
+                modified_files=modified_files,
+            )
+        )
+
+        for cu, solc_output in out:
+            self.__errors.update(solc_output.errors)
+            errored = any(
+                error.severity == SolcOutputErrorSeverityEnum.ERROR
+                for error in solc_output.errors
+            )
+
+            for source_unit_name, raw_ast in solc_output.sources.items():
+                if errored:
+                    # an error occurred during compilation
+                    # AST still may be provided, but it must NOT be parsed (pydantic model is not defined for this case)
+                    path = cu.source_unit_name_to_path(source_unit_name)
+                    if path in modified_files:
+                        self.__asts.pop(path)
+                        self.__source_units.pop(path)
+                else:
+                    path = cu.source_unit_name_to_path(source_unit_name)
+                    ast = AstSolc.parse_obj(raw_ast.ast)
+                    self.__asts[path] = ast
+                    self.__source_units[path] = SourceUnit(
+                        path, ast, self.get_file_content(path).encode("utf-8"), cu
+                    )
+
     def __compilation_loop(self):
         if sys.version_info < (3, 8):
             loop = asyncio.new_event_loop()
@@ -129,12 +178,8 @@ class LspCompiler:
                 self.__files[file.resolve()] = file.read_text()
 
         # perform initial compilation
-        out = asyncio.run(
-            self.__compiler.compile(
-                self.__files.keys(), [SolcOutputSelectionEnum.AST], False, False
-            )
-        )
-        self.__output = list(out)
+        self.__compile(self.__files.keys(), {})
+
         self.__output_contents = self.__files.copy()
         self.output_ready.set()
 
@@ -200,12 +245,8 @@ class LspCompiler:
                 modified_files = {
                     path: self.__files[path] for path in self.__modified_files
                 }
-                out = asyncio.run(
-                    self.__compiler.compile(
-                        {}, [SolcOutputSelectionEnum.AST], False, False, modified_files
-                    )
-                )
-                self.__output = list(out)
+                self.__compile([], modified_files)
+
                 self.__output_contents.update(self.__files)
                 self.__modified_files.clear()
 
