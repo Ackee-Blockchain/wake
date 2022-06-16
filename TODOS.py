b743a396722e9a00604452e07ba5083b766702ee
Michal Převrátil <michprev@gmail.com>
2022-06-16 16:32:03 +0200

    :fire: Remove `PLANNING.adoc` and `TODOS.py`

--- TODOS.py
+++
@@ -1,4 +0,0 @@
-# todos that don't fit anywhere else (because of the limited set of supported formats:
-# https://github.com/alstr/todo-to-issue-action#supported-languages
-# Using python extension here bc it has simpler comment format than markdown :D
-
