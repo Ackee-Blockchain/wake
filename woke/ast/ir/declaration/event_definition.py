51a5866c0ca1c97af9882c9f3c10433d3ae270b5
Michal Převrátil <michprev@gmail.com>
2022-06-19 10:51:57 +0200

    :sparkles: Add event definition IR

---
+++ woke/ast/ir/declaration/event_definition.py
@@ -0,0 +1,45 @@
+from typing import Optional
+
+from woke.ast.ir.abc import IrAbc
+from woke.ast.ir.meta.parameter_list import ParameterList
+from woke.ast.ir.meta.structured_documentation import StructuredDocumentation
+from woke.ast.ir.utils import IrInitTuple
+from woke.ast.nodes import SolcEventDefinition
+
+
+class EventDefinition(IrAbc):
+    _ast_node: SolcEventDefinition
+    # _parent: ContractDefinition
+
+    __name: str
+    __anonymous: bool
+    __parameters: ParameterList
+    __documentation: Optional[StructuredDocumentation]
+
+    def __init__(self, init: IrInitTuple, event: SolcEventDefinition, parent: IrAbc):
+        super().__init__(init, event, parent)
+        self.__name = event.name
+        self.__anonymous = event.anonymous
+        self.__parameters = ParameterList(init, event.parameters, self)
+        self.__documentation = (
+            StructuredDocumentation(init, event.documentation, self)
+            if event.documentation
+            else None
+        )
+        # TODO event selector?
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @property
+    def anonymous(self) -> bool:
+        return self.__anonymous
+
+    @property
+    def parameters(self) -> ParameterList:
+        return self.__parameters
+
+    @property
+    def documentation(self) -> Optional[StructuredDocumentation]:
+        return self.__documentation
