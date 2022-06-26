5e8a913c2656ca0280eadf2a0db7df432d970d61
Michal Převrátil <michprev@gmail.com>
2022-06-26 23:06:33 +0200

    :sparkles: Make `ReferenceResolver` callbacks prioritizable

--- woke/ast/ir/reference_resolver.py
+++ woke/ast/ir/reference_resolver.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from pathlib import Path
+from queue import PriorityQueue
 from typing import TYPE_CHECKING, Callable, Dict, List
 
 if TYPE_CHECKING:
@@ -16,13 +17,19 @@ class CallbackParams:
     source_units: Dict[Path, SourceUnit]
 
 
+@dataclass(order=True)
+class PostProcessQueueItem:
+    priority: int
+    callback: Callable[[CallbackParams], None] = field(compare=False)
+
+
 class ReferenceResolver:
     __registered_nodes: Dict[bytes, Dict[AstNodeId, IrAbc]]
-    __post_process_callbacks: List[Callable[[CallbackParams], None]]
+    __post_process_callbacks: PriorityQueue[PostProcessQueueItem]
 
     def __init__(self):
         self.__registered_nodes = {}
-        self.__post_process_callbacks = []
+        self.__post_process_callbacks = PriorityQueue()
 
     def register_node(self, node: IrAbc, node_id: AstNodeId, cu_hash: bytes):
         if cu_hash not in self.__registered_nodes:
@@ -33,11 +40,11 @@ class ReferenceResolver:
         return self.__registered_nodes[cu_hash][node_id]
 
     def register_post_process_callback(
-        self, callback: Callable[[CallbackParams], None]
+        self, callback: Callable[[CallbackParams], None], priority: int = 0
     ):
-        self.__post_process_callbacks.append(callback)
+        self.__post_process_callbacks.put(PostProcessQueueItem(priority, callback))
 
     def run_post_process_callbacks(self, callback_params: CallbackParams):
-        for callback in self.__post_process_callbacks:
+        while not self.__post_process_callbacks.empty():
+            callback = self.__post_process_callbacks.get().callback
             callback(callback_params)
-        self.__post_process_callbacks.clear()
