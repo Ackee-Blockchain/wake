398340151efc3b3857bbc3d6d4ac5645cabab752
Michal Převrátil <michprev@gmail.com>
2022-06-27 11:23:59 +0200

    :sparkles: Implement `Find references` for global symbols

--- woke/lsp/features/references.py
+++ woke/lsp/features/references.py
@@ -1,6 +1,7 @@
 import logging
-from typing import List, Union
+from typing import Iterable, List, Union
 
+from woke.ast.enums import GlobalSymbolsEnum
 from woke.ast.ir.abc import IrAbc
 from woke.ast.ir.declaration.abc import DeclarationAbc
 from woke.ast.ir.declaration.function_definition import FunctionDefinition
@@ -47,6 +48,37 @@ class ReferenceParams(
 
 
 def _generate_references(
+    node_references: Iterable[
+        Union[Identifier, IdentifierPath, MemberAccess, UserDefinedTypeName]
+    ],
+    context: LspContext,
+) -> List[Location]:
+    refs = []
+    for reference in node_references:
+        path = reference.file
+        uri = path_to_uri(path)
+
+        byte_start, byte_end = reference.byte_location
+        start_line, start_column = context.compiler.get_line_pos_from_byte_offset(
+            path, byte_start
+        )
+        end_line, end_column = context.compiler.get_line_pos_from_byte_offset(
+            path, byte_end
+        )
+
+        refs.append(
+            Location(
+                uri=DocumentUri(uri),
+                range=Range(
+                    start=Position(line=start_line, character=start_column),
+                    end=Position(line=end_line, character=end_column),
+                ),
+            )
+        )
+    return refs
+
+
+def _generate_declaration_references(
     declaration: DeclarationAbc, include_declaration: bool, context: LspContext
 ) -> List[Location]:
     refs = []
@@ -70,27 +102,7 @@ def _generate_references(
             )
         )
 
-    for reference in declaration.references:
-        path = reference.file
-        uri = path_to_uri(path)
-
-        byte_start, byte_end = reference.byte_location
-        start_line, start_column = context.compiler.get_line_pos_from_byte_offset(
-            path, byte_start
-        )
-        end_line, end_column = context.compiler.get_line_pos_from_byte_offset(
-            path, byte_end
-        )
-
-        refs.append(
-            Location(
-                uri=DocumentUri(uri),
-                range=Range(
-                    start=Position(line=start_line, character=start_column),
-                    end=Position(line=end_line, character=end_column),
-                ),
-            )
-        )
+    refs.extend(_generate_references(declaration.references, context))
     return refs
 
 
@@ -123,7 +135,17 @@ def references(
             referenced_declaration = node.referenced_declaration
             if referenced_declaration is None:
                 return None
-            node = referenced_declaration
+            if isinstance(referenced_declaration, GlobalSymbolsEnum):
+                global_refs = (
+                    context.compiler.ir_reference_resolver.get_global_symbol_references(
+                        referenced_declaration
+                    )
+                )
+                if len(global_refs) > 0:
+                    return _generate_references(global_refs, context)
+                return None
+            else:
+                node = referenced_declaration
 
         if not isinstance(node, DeclarationAbc):
             return None
@@ -134,7 +156,7 @@ def references(
         while declarations_queue:
             declaration = declarations_queue.pop()
             refs.extend(
-                _generate_references(
+                _generate_declaration_references(
                     declaration, params.context.include_declaration, context
                 )
             )
