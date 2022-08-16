d261954c5e37ab672e4108cef7d2667c7e7343a6
Michal Převrátil <michprev@gmail.com>
2022-08-16 20:34:40 +0200

    ✨ Show OpenZeppelin docs links in LSP hover

--- woke/lsp/features/hover.py
+++ woke/lsp/features/hover.py
@@ -3,11 +3,13 @@ from typing import List, Optional, Tuple, Union
 
 from ...ast.ir.abc import IrAbc
 from ...ast.ir.declaration.abc import DeclarationAbc
+from ...ast.ir.declaration.contract_definition import ContractDefinition
 from ...ast.ir.expression.identifier import Identifier
 from ...ast.ir.expression.member_access import MemberAccess
 from ...ast.ir.meta.identifier_path import IdentifierPath
 from ...ast.ir.statement.inline_assembly import InlineAssembly
 from ...ast.ir.type_name.user_defined_type_name import UserDefinedTypeName
+from ...core.solidity_version import SemanticVersion
 from ..common_structures import (
     MarkupContent,
     MarkupKind,
@@ -52,6 +54,92 @@ MarkedStringType = Union[str, MarkedString]
 class Hover(LspModel):
     contents: Union[MarkedStringType, List[MarkedStringType], MarkupContent]
     range: Optional[Range]
+
+
+def _append_openzeppelin_docs(
+    node: ContractDefinition, version: SemanticVersion
+) -> str:
+    source_unit = node.parent
+    version_string = f"{version.major}.x"
+    url_base = f"https://docs.openzeppelin.com/contracts/{version_string}"
+    doc_url = None
+    api_doc_url = None
+
+    if "openzeppelin/contracts/access" in source_unit.source_unit_name:
+        doc_url = "access-control"
+        api_doc_url = "access"
+    elif "openzeppelin/contracts/crosschain" in source_unit.source_unit_name:
+        doc_url = "crosschain"
+        api_doc_url = "crosschain"
+    elif "openzeppelin/contracts/finance" in source_unit.source_unit_name:
+        if node.name == "PaymentSplitter":
+            doc_url = "utilities#payment"
+        api_doc_url = "finance"
+    elif "openzeppelin/contracts/governance" in source_unit.source_unit_name:
+        doc_url = "governance"
+        api_doc_url = "governance"
+    elif (
+        "openzeppelin/contracts/interfaces" in source_unit.source_unit_name
+        or "openzeppelin/contracts/token" in source_unit.source_unit_name
+    ):
+        if "ERC20" in node.name:
+            doc_url = "erc20"
+            api_doc_url = "token/erc20"
+        elif "ERC721" in node.name:
+            doc_url = "erc721"
+            api_doc_url = "token/erc721"
+        elif "ERC777" in node.name:
+            doc_url = "erc777"
+            api_doc_url = "token/erc777"
+        elif "ERC1155" in node.name:
+            doc_url = "erc1155"
+            api_doc_url = "token/erc1155"
+        elif node.name.startswith("I"):
+            api_doc_url = "interfaces"
+        else:
+            api_doc_url = "token/common"
+    elif "openzeppelin/contracts/metatx" in source_unit.source_unit_name:
+        api_doc_url = "metatx"
+    elif "openzeppelin/contracts/proxy" in source_unit.source_unit_name:
+        api_doc_url = "proxy"
+    elif "openzeppelin/contracts/security" in source_unit.source_unit_name:
+        if node.name == "PullPayment":
+            doc_url = "utilities#payment"
+        api_doc_url = "security"
+    elif "openzeppelin/contracts/utils" in source_unit.source_unit_name:
+        doc_url = "utilities"
+        api_doc_url = "utils"
+    elif "openzeppelin/contracts/GSN" in source_unit.source_unit_name:
+        doc_url = "gsn"
+        api_doc_url = "gsn"
+    elif "openzeppelin/contracts/cryptography" in source_unit.source_unit_name:
+        doc_url = "utilities#cryptography"
+        api_doc_url = "cryptography"
+    elif "openzeppelin/contracts/drafts" in source_unit.source_unit_name:
+        api_doc_url = "drafts"
+    elif "openzeppelin/contracts/math" in source_unit.source_unit_name:
+        doc_url = "utilities#math"
+        api_doc_url = "math"
+    elif "openzeppelin/contracts/payment" in source_unit.source_unit_name:
+        doc_url = "utilities#payment"
+        api_doc_url = "payment"
+    elif "openzeppelin/contracts/presets" in source_unit.source_unit_name:
+        if "ERC20" in node.name:
+            doc_url = "erc20#Presets"
+        elif "ERC721" in node.name:
+            doc_url = "erc721#Presets"
+        elif "ERC777" in node.name:
+            doc_url = "erc777#Presets"
+        elif "ERC1155" in node.name:
+            doc_url = "erc1155#Presets"
+        api_doc_url = "presets"
+
+    ret = ""
+    if doc_url is not None:
+        ret += f"\n\n[OpenZeppelin documentation]({url_base}/{doc_url})"
+    if api_doc_url is not None:
+        ret += f"\n\n[OpenZeppelin API documentation]({url_base}/api/{api_doc_url}#{node.name})"
+    return ret
 
 
 async def hover(context: LspContext, params: HoverParams) -> Optional[Hover]:
@@ -106,10 +194,18 @@ async def hover(context: LspContext, params: HoverParams) -> Optional[Hover]:
     if not isinstance(node, DeclarationAbc):
         return None
 
+    value = "```solidity\n" + node.declaration_string + "\n```"
+    if (
+        isinstance(node, ContractDefinition)
+        and context.openzeppelin_contracts_version is not None
+        and context.openzeppelin_contracts_version >= "2.0.0"
+    ):
+        value += _append_openzeppelin_docs(node, context.openzeppelin_contracts_version)
+
     return Hover(
         contents=MarkupContent(
             kind=MarkupKind.MARKDOWN,
-            value="```solidity\n" + node.declaration_string + "\n```",
+            value=value,
         ),
         range=context.compiler.get_range_from_byte_offsets(
             path, original_node_location
