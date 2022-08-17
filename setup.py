cfdd09b5b662964fa4d2462ceb955670afe777c0
Michal Převrátil <michprev@gmail.com>
2022-08-17 12:03:32 +0200

    🔧 Exclude `tests` dir from packages in `setup.py`

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     url="https://github.com/Ackee-Blockchain/woke",
     author="Ackee Blockchain",
     version="0.1.3-rc3",
-    packages=find_packages(),
+    packages=find_packages(exclude=("tests",)),
     python_requires=">=3.7",
     install_requires=install_requires,
     extras_require=extras_require,
