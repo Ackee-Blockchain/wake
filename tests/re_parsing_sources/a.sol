3b5785c638d5b0b4589ccbe585b31a965656b966
Michal Převrátil <michprev@gmail.com>
2022-08-21 21:19:07 +0200

    🐛 Fix import directive regex parsing

--- tests/re_parsing_sources/a.sol
+++ tests/re_parsing_sources/a.sol
@@ -6,6 +6,7 @@ should be ignored pragma solidity 0.8.6;
 */
 
 contract a {
+    string imported;
     string malicious = '"should be \' ignored;pragma solidity 0.8.10;';
     string x = "123"; string malicious2 = 'should be ignored " \'pragma solidity 0.8.4;';
     string y = "123"; string malicious3 = "should be ignored \" \'pragma solidity 0.8.3;";
