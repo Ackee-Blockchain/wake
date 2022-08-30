c08f428558bd84a4cb8852895a0c855afe2b92ec
Mirek Škrabal <mirekskrabal94@gmail.com>
2022-08-30 11:12:21 +0200

    🐛 fix inheritance of contracts in the same src unit

---
+++ woke/fuzzer/constants.py
@@ -0,0 +1,16 @@
+#tab space width for indentation
+TAB_WIDTH = 4
+
+#TODO move to constants file
+DEFAULT_IMPORTS: str = """
+import random 
+from dataclasses import dataclass 
+from typing import List, NewType, Optional
+
+from woke.fuzzer.contract import Contract
+
+from eth_typing import AnyAddress, HexStr
+from web3 import Web3, WebsocketProvider, HTTPProvider
+from web3.method import Method
+from web3.types import TxParams, Address, RPCEndpoint
+"""
