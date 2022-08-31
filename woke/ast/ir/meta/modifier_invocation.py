2ecb4a4c271fbcb674c8882ec2b93621db204bba
Michal Převrátil <michprev@gmail.com>
2022-08-31 13:48:34 +0200

    ✨ Add `kind` prop to `ModifierInvocation` IR node

--- woke/ast/ir/meta/modifier_invocation.py
+++ woke/ast/ir/meta/modifier_invocation.py
@@ -5,6 +5,8 @@ from typing import TYPE_CHECKING, Iterator, List, Optional, Union
 from ..expression.abc import ExpressionAbc
 from ..expression.identifier import Identifier
 from .identifier_path import IdentifierPath
+from ..reference_resolver import CallbackParams
+from ...enums import ModifierInvocationKind
 
 if TYPE_CHECKING:
     from ..declaration.function_definition import FunctionDefinition
@@ -18,6 +20,7 @@ class ModifierInvocation(SolidityAbc):
     _ast_node: SolcModifierInvocation
     _parent: FunctionDefinition
 
+    __kind: Optional[ModifierInvocationKind]
     __modifier_name: Union[Identifier, IdentifierPath]
     __arguments: Optional[List[ExpressionAbc]]
 
@@ -28,6 +31,7 @@ class ModifierInvocation(SolidityAbc):
         parent: SolidityAbc,
     ):
         super().__init__(init, modifier_invocation, parent)
+        self.__kind = None
         if isinstance(modifier_invocation.modifier_name, SolcIdentifier):
             self.__modifier_name = Identifier(
                 init, modifier_invocation.modifier_name, self
@@ -45,6 +49,20 @@ class ModifierInvocation(SolidityAbc):
                 for argument in modifier_invocation.arguments
             ]
 
+        self._reference_resolver.register_post_process_callback(self.__post_process)
+
+    def __post_process(self, callback_params: CallbackParams):
+        from ..declaration.contract_definition import ContractDefinition
+        from ..declaration.modifier_definition import ModifierDefinition
+
+        referenced_declaration = self.modifier_name.referenced_declaration
+        if isinstance(referenced_declaration, ModifierDefinition):
+            self.__kind = ModifierInvocationKind.MODIFIER_INVOCATION
+        elif isinstance(referenced_declaration, ContractDefinition):
+            self.__kind = ModifierInvocationKind.BASE_CONSTRUCTOR_SPECIFIER
+        else:
+            assert False, f"Unexpected declaration type: {referenced_declaration}"
+
     def __iter__(self) -> Iterator[IrAbc]:
         yield self
         yield from self.__modifier_name
@@ -55,6 +73,11 @@ class ModifierInvocation(SolidityAbc):
     @property
     def parent(self) -> FunctionDefinition:
         return self._parent
+
+    @property
+    def kind(self) -> ModifierInvocationKind:
+        assert self.__kind is not None
+        return self.__kind
 
     @property
     def modifier_name(self) -> Union[Identifier, IdentifierPath]:
