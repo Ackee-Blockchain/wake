1ef878fcd0b56119a4bb9ab69306d0a2a7b4e72f
Michal Převrátil <michprev@gmail.com>
2022-09-02 10:32:45 +0100

    📝 Add `Enums` API reference page

--- woke/ast/enums.py
+++ woke/ast/enums.py
@@ -2,6 +2,11 @@ import enum
 
 
 class GlobalSymbolsEnum(enum.IntEnum):
+    """
+    Global symbols of the Solidity language. Symbols with identifiers from `-1` to `-99` are codified by the compiler and can only be referenced by [Identifier][woke.ast.ir.expression.identifier.Identifier] nodes.
+    Other symbols are not officially codified by the compiler, but Woke also defines identifiers for them. These symbols can only be referenced by [MemberAccess][woke.ast.ir.expression.member_access.MemberAccess] nodes.
+    See the [Solidity docs](https://docs.soliditylang.org/en/latest/units-and-global-variables.html#special-variables-and-functions) for (an incomplete) list of global symbols and their descriptions.
+    """
     ABI = -1
     ADDMOD = -2
     ASSERT = -3
@@ -83,6 +88,9 @@ class GlobalSymbolsEnum(enum.IntEnum):
 
 # Contracts
 class ContractKind(str, enum.Enum):
+    """
+    Kind of a [ContractDefinition][woke.ast.ir.declaration.contract_definition.ContractDefinition] declaration node.
+    """
     CONTRACT = "contract"
     INTERFACE = "interface"
     LIBRARY = "library"
@@ -90,6 +98,9 @@ class ContractKind(str, enum.Enum):
 
 # State variables
 class Mutability(str, enum.Enum):
+    """
+    Mutability of a [VariableDeclaration][woke.ast.ir.declaration.variable_declaration.VariableDeclaration] declaration node.
+    """
     MUTABLE = "mutable"
     IMMUTABLE = "immutable"
     CONSTANT = "constant"
@@ -97,14 +108,26 @@ class Mutability(str, enum.Enum):
 
 # Functions
 class FunctionKind(str, enum.Enum):
+    """
+    Kind of a [FunctionDefinition][woke.ast.ir.declaration.function_definition.FunctionDefinition] declaration node.
+    """
     FUNCTION = "function"
     RECEIVE = "receive"
     CONSTRUCTOR = "constructor"
     FALLBACK = "fallback"
     FREE_FUNCTION = "freeFunction"
+    """
+    Function defined outside of a contract.
+    """
 
 
 class Visibility(str, enum.Enum):
+    """
+    Visibility of:
+
+    - [FunctionTypeName][woke.ast.ir.type_name.function_type_name.FunctionTypeName] type name,
+    - [FunctionDefinition][woke.ast.ir.declaration.function_definition.FunctionDefinition], [ModifierDefinition][woke.ast.ir.declaration.modifier_definition.ModifierDefinition], [StructDefinition][woke.ast.ir.declaration.struct_definition.StructDefinition] and [VariableDeclaration][woke.ast.ir.declaration.variable_declaration.VariableDeclaration] declarations.
+    """
     EXTERNAL = "external"
     PUBLIC = "public"
     INTERNAL = "internal"
@@ -112,6 +135,15 @@ class Visibility(str, enum.Enum):
 
 
 class StateMutability(str, enum.Enum):
+    """
+    State mutability of:
+
+    - [Function][woke.ast.expression_types.Function] expression type,
+    - [FunctionDefinition][woke.ast.ir.declaration.function_definition.FunctionDefinition] declaration,
+    - [ElementaryTypeName][woke.ast.ir.type_name.elementary_type_name.ElementaryTypeName] and [FunctionTypeName][woke.ast.ir.type_name.function_type_name.FunctionTypeName] type names.
+
+    In the case of [ElementaryTypeName][woke.ast.ir.type_name.elementary_type_name.ElementaryTypeName], the state mutability is specified only for the `address` type and can be either [NONPAYABLE][woke.ast.enums.StateMutability.NONPAYABLE] or [PAYABLE][woke.ast.enums.StateMutability.PAYABLE].
+    """
     PAYABLE = "payable"
     PURE = "pure"
     NONPAYABLE = "nonpayable"
@@ -119,12 +151,18 @@ class StateMutability(str, enum.Enum):
 
 
 class ModifierInvocationKind(str, enum.Enum):
+    """
+    Kind of a [ModifierInvocation][woke.ast.ir.meta.modifier_invocation.ModifierInvocation] meta node.
+    """
     MODIFIER_INVOCATION = "modifierInvocation"
     BASE_CONSTRUCTOR_SPECIFIER = "baseConstructorSpecifier"
 
 
 # Literals
 class LiteralKind(str, enum.Enum):
+    """
+    Kind of a [Literal][woke.ast.ir.expression.literal.Literal] expression node.
+    """
     BOOL = "bool"
     NUMBER = "number"
     STRING = "string"
@@ -133,12 +171,9 @@ class LiteralKind(str, enum.Enum):
 
 
 class YulLiteralValueKind(str, enum.Enum):
-    NUMBER = "number"
-    STRING = "string"
-    BOOL = "bool"
-
-
-class YulLiteralHexValueKind(str, enum.Enum):
+    """
+    Kind of a Yul [Literal][woke.ast.ir.yul.literal.Literal] node.
+    """
     NUMBER = "number"
     STRING = "string"
     BOOL = "bool"
@@ -146,21 +181,42 @@ class YulLiteralHexValueKind(str, enum.Enum):
 
 # Function call
 class FunctionCallKind(str, enum.Enum):
+    """
+    Kind of a [FunctionCall][woke.ast.ir.expression.function_call.FunctionCall] expression node.
+    """
     FUNCTION_CALL = "functionCall"
+    """
+    Represents also an error call, event call and [NewExpression][woke.ast.ir.expression.new_expression.NewExpression] call.
+    """
     TYPE_CONVERSION = "typeConversion"
     STRUCT_CONSTRUCTOR_CALL = "structConstructorCall"
 
 
 # Compound types
 class DataLocation(str, enum.Enum):
+    """
+    Data location of a [VariableDeclaration][woke.ast.ir.declaration.variable_declaration.VariableDeclaration] node.
+    It also specifies the data location of the following expression types:
+
+    - [Array][woke.ast.expression_types.Array],
+    - [Bytes][woke.ast.expression_types.Bytes],
+    - [String][woke.ast.expression_types.String],
+    - [Struct][woke.ast.expression_types.Struct].
+    """
     CALLDATA = "calldata"
     DEFAULT = "default"
+    """
+    Set only in [VariableDeclaration][woke.ast.ir.declaration.variable_declaration.VariableDeclaration] nodes when the data location is not specified (and the compiler even does not allow it).
+    """
     MEMORY = "memory"
     STORAGE = "storage"
 
 
 # Operations
 class UnaryOpOperator(str, enum.Enum):
+    """
+    Unary operation operator used in an [UnaryOperation][woke.ast.ir.expression.unary_operation.UnaryOperation] expression.
+    """
     PLUS_PLUS = r"++"
     MINUS_MINUS = r"--"
     MINUS = r"-"
@@ -170,6 +226,9 @@ class UnaryOpOperator(str, enum.Enum):
 
 
 class AssignmentOperator(str, enum.Enum):
+    """
+    Assignment operator used in an [Assignment][woke.ast.ir.expression.assignment.Assignment] expression.
+    """
     EQUALS = r"="
     PLUS_EQUALS = r"+="
     MINUS_EQUALS = r"-="
@@ -184,6 +243,9 @@ class AssignmentOperator(str, enum.Enum):
 
 
 class BinaryOpOperator(str, enum.Enum):
+    """
+    Binary operation operator used in a [BinaryOperation][woke.ast.ir.expression.binary_operation.BinaryOperation] expression.
+    """
     PLUS = r"+"
     MINUS = r"-"
     TIMES = r"*"
@@ -231,6 +293,9 @@ class InlineAssemblyFlag(str, enum.Enum):
 
 
 class ModifiesStateFlag(enum.IntFlag):
+    """
+    Flag enum describing how an expression ([ExpressionAbc][woke.ast.ir.expression.abc.ExpressionAbc]) or statement ([StatementAbc][woke.ast.ir.statement.abc.StatementAbc]) modifies the blockchain state.
+    """
     MODIFIES_STATE_VAR = 1
     EMITS = 2
     SENDS_ETHER = 4
@@ -248,3 +313,61 @@ class ModifiesStateFlag(enum.IntFlag):
         return " | ".join(f.name or "" for f in flags)
 
     __str__ = __repr__
+
+
+class FunctionExpressionKind(str, enum.Enum):
+    """
+    Kind of a [Function][woke.ast.expression_types.Function] expression type.
+    """
+    DECLARATION = "declaration"
+    INTERNAL = "internal"
+    EXTERNAL = "external"
+    DELEGATE_CALL = "delegatecall"
+    BARE_CALL = "barecall"
+    BARE_CALL_CODE = "barecallcode"
+    BARE_DELEGATE_CALL = "baredelegatecall"
+    BARE_STATIC_CALL = "barestaticcall"
+    CREATION = "creation"
+    SEND = "send"
+    TRANSFER = "transfer"
+    KECCAK256 = "keccak256"
+    SELFDESCTRUCT = "selfdestruct"
+    REVERT = "revert"
+    EC_RECOVER = "ecrecover"
+    SHA256 = "sha256"
+    RIPEMD160 = "ripemd160"
+    GAS_LEFT = "gasleft"
+    EVENT = "event"
+    ERROR = "error"
+    WRAP = "wrap"
+    UNWRAP = "unwrap"
+    SET_GAS = "setgas"
+    SET_VALUE = "setvalue"
+    BLOCK_HASH = "blockhash"
+    ADD_MOD = "addmod"
+    MUL_MOD = "mulmod"
+    ARRAY_PUSH = "arraypush"
+    ARRAY_POP = "arraypop"
+    BYTES_CONCAT = "bytesconcat"
+    STRING_CONCAT = "stringconcat"
+    OBJECT_CREATION = "objectcreation"
+    ASSERT = "assert"
+    REQUIRE = "require"
+    ABI_ENCODE = "abiencode"
+    ABI_ENCODE_PACKED = "abiencodepacked"
+    ABI_ENCODE_WITH_SELECTOR = "abiencodewithselector"
+    ABI_ENCODE_CALL = "abiencodecall"
+    ABI_ENCODE_WITH_SIGNATURE = "abiencodewithsignature"
+    ABI_DECODE = "abidecode"
+    META_TYPE = "metatype"
+
+
+class MagicExpressionKind(str, enum.Enum):
+    """
+    Kind of a [Magic][woke.ast.expression_types.Magic] expression type.
+    """
+    BLOCK = "block"
+    MESSAGE = "message"
+    TRANSACTION = "transaction"
+    ABI = "abi"
+    META_TYPE = "meta_type"
