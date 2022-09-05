e9455c5a4a33e245f547c284385c3b7f11878033
Michal Převrátil <michprev@gmail.com>
2022-09-05 21:26:32 +0100

    🚧 Specify IR type names `parent` and `type`

--- woke/ast/ir/type_name/elementary_type_name.py
+++ woke/ast/ir/type_name/elementary_type_name.py
@@ -1,4 +1,17 @@
-from typing import Optional
+from __future__ import annotations
+
+from typing import Optional, Union, TYPE_CHECKING
+
+from ...expression_types import Address, Bool, Int, UInt, String, Bytes, FixedBytes, Type, Fixed, UFixed
+
+if TYPE_CHECKING:
+    from ..declaration.user_defined_value_type_definition import UserDefinedValueTypeDefinition
+    from ..declaration.variable_declaration import VariableDeclaration
+    from ..expression.elementary_type_name_expression import ElementaryTypeNameExpression
+    from ..expression.new_expression import NewExpression
+    from ..meta.using_for_directive import UsingForDirective
+    from .array_type_name import ArrayTypeName
+    from .mapping import Mapping
 
 from woke.ast.enums import StateMutability
 from woke.ast.ir.abc import IrAbc, SolidityAbc
@@ -9,7 +22,7 @@ from woke.ast.nodes import SolcElementaryTypeName
 
 class ElementaryTypeName(TypeNameAbc):
     _ast_node: SolcElementaryTypeName
-    _parent: SolidityAbc  # TODO: make this more specific
+    _parent: Union[VariableDeclaration, UserDefinedValueTypeDefinition, ElementaryTypeNameExpression, NewExpression, UsingForDirective, ArrayTypeName, Mapping]
 
     __name: str
     __state_mutability: Optional[StateMutability]
@@ -35,8 +48,16 @@ class ElementaryTypeName(TypeNameAbc):
             self._type_descriptions = parent._type_descriptions
 
     @property
-    def parent(self) -> SolidityAbc:
+    def parent(self) -> Union[VariableDeclaration, UserDefinedValueTypeDefinition, ElementaryTypeNameExpression, NewExpression, UsingForDirective, ArrayTypeName, Mapping]:
         return self._parent
+
+    @property
+    def type(self) -> Union[Address, Bool, Int, UInt, Fixed, UFixed, String, Bytes, FixedBytes, Type]:
+        t = super().type
+        if not isinstance(t, (Address, Bool, Int, UInt, Fixed, UFixed, String, Bytes, FixedBytes, Type)):
+            raise TypeError(f"Unexpected type {t} {self.source}")
+        assert isinstance(t, (Address, Bool, Int, UInt, Fixed, UFixed, String, Bytes, FixedBytes, Type))
+        return t
 
     @property
     def name(self) -> str:
