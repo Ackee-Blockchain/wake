f7da8696ca3c1866be5ac6c3ad8cd1d0b83ca4c0
Michal Převrátil <michprev@gmail.com>
2022-09-07 10:25:34 +0100

    📝 Add API ref pages for type name IR nodes

--- woke/ast/ir/type_name/array_type_name.py
+++ woke/ast/ir/type_name/array_type_name.py
@@ -18,6 +18,47 @@ from woke.ast.nodes import SolcArrayTypeName
 
 
 class ArrayTypeName(TypeNameAbc):
+    """
+    Array type name.
+
+    !!! example
+        An array type name can be used:
+
+        - inside a [VariableDeclaration][woke.ast.ir.declaration.variable_declaration.VariableDeclaration]:
+            - `:::solidity bool[]` in line 1,
+            - `:::solidity int[10][20]` in line 11,
+            - `:::solidity string[10]` in line 12,
+            - `:::solidity address[]` in line 16,
+        - inside a [NewExpression][woke.ast.ir.expression.new_expression.NewExpression]:
+            - `:::solidity address[]` in line 16,
+        - inside a [UsingForDirective][woke.ast.ir.meta.using_for_directive.UsingForDirective]:
+            - `:::solidity bool[]` in line 8,
+        - inside an [ArrayTypeName][woke.ast.ir.type_name.array_type_name.ArrayTypeName]:
+            - `:::solidity int[10]` in line 11,
+        - inside a [Mapping][woke.ast.ir.type_name.mapping.Mapping]:
+            - `:::solidity C[]` in line 13.
+
+        ```solidity linenums="1"
+        function or(bool[] memory arr) pure returns(bool) {
+            for (uint i = 0; i < arr.length; i++)
+                if (arr[i])
+                    return true;
+            return false;
+        }
+
+        using {or} for bool[];
+
+        contract C {
+            int[10][20] arr;
+            string[10] names;
+            mapping(address => C[]) map;
+
+            function foo() public pure {
+                address[] memory addresses = new address[](5);
+            }
+        }
+        ```
+    """
     _ast_node: SolcArrayTypeName
     _parent: Union[VariableDeclaration, NewExpression, UsingForDirective, ArrayTypeName, Mapping]
 
@@ -43,18 +84,39 @@ class ArrayTypeName(TypeNameAbc):
 
     @property
     def parent(self) -> Union[VariableDeclaration, NewExpression, UsingForDirective, ArrayTypeName, Mapping]:
+        """
+        Returns:
+            Parent IR node.
+        """
         return self._parent
 
     @property
     def type(self) -> Array:
+        """
+        Returns:
+            Type description.
+        """
         t = super().type
         assert isinstance(t, Array)
         return t
 
     @property
     def base_type(self) -> TypeNameAbc:
+        """
+        !!! example
+            `uint8[2]` has `uint8` ([ElementaryTypeName][woke.ast.ir.type_name.elementary_type_name.ElementaryTypeName]) as a base type.
+
+            `uint8[2][3]` has `uint8[2]` ([ArrayTypeName][woke.ast.ir.type_name.array_type_name.ArrayTypeName]) as a base type.
+        Returns:
+            Type name IR node describing the base type.
+        """
         return self.__base_type
 
     @property
     def length(self) -> Optional[ExpressionAbc]:
+        """
+        Returns an expression as present in the source code.
+        Returns:
+            Expression defining the length of the array.
+        """
         return self.__length
--- woke/ast/ir/type_name/mapping.py
+++ woke/ast/ir/type_name/mapping.py
@@ -2,6 +2,9 @@ from __future__ import annotations
 
 from typing import Iterator, Union, TYPE_CHECKING
 
+from .elementary_type_name import ElementaryTypeName
+from .user_defined_type_name import UserDefinedTypeName
+
 if TYPE_CHECKING:
     from ..declaration.variable_declaration import VariableDeclaration
     from ..meta.using_for_directive import UsingForDirective
@@ -15,15 +18,46 @@ from woke.ast.nodes import SolcMapping
 
 
 class Mapping(TypeNameAbc):
+    """
+    Mapping type name.
+
+    !!! example
+        A mapping type name can be used:
+
+        - inside a [VariableDeclaration][woke.ast.ir.declaration.variable_declaration.VariableDeclaration]:
+            - `:::solidity mapping(address => uint)` in line 1,
+            - `:::solidity mapping(address => mapping(address => uint))` in line 8,
+        - inside a [UsingForDirective][woke.ast.ir.meta.using_for_directive.UsingForDirective]:
+            - `:::solidity mapping(address => uint)` in line 5,
+        - inside an [ArrayTypeName][woke.ast.ir.type_name.array_type_name.ArrayTypeName]:
+            - `:::solidity mapping(address => uint)` in line 9,
+        - inside a [Mapping][woke.ast.ir.type_name.mapping.Mapping]:
+            - `:::solidity mapping(address => uint)` in line 8.
+
+        ```solidity linenums="1"
+        function remove(mapping(address => uint) storage balances, address account) {
+            delete balances[account];
+        }
+
+        using {remove} for mapping(address => uint);
+
+        contract C {
+            mapping(address => mapping(address => uint)) public allowances;
+            mapping(address => uint)[2] public balances;
+        }
+        ```
+    """
     _ast_node: SolcMapping
     _parent: Union[VariableDeclaration, UsingForDirective, ArrayTypeName, Mapping]
 
-    __key_type: TypeNameAbc
+    __key_type: Union[ElementaryTypeName, UserDefinedTypeName]
     __value_type: TypeNameAbc
 
     def __init__(self, init: IrInitTuple, mapping: SolcMapping, parent: SolidityAbc):
         super().__init__(init, mapping, parent)
-        self.__key_type = TypeNameAbc.from_ast(init, mapping.key_type, self)
+        key_type = TypeNameAbc.from_ast(init, mapping.key_type, self)
+        assert isinstance(key_type, (ElementaryTypeName, UserDefinedTypeName))
+        self.__key_type = key_type
         self.__value_type = TypeNameAbc.from_ast(init, mapping.value_type, self)
 
     def __iter__(self) -> Iterator[IrAbc]:
@@ -33,18 +67,40 @@ class Mapping(TypeNameAbc):
 
     @property
     def parent(self) -> Union[VariableDeclaration, UsingForDirective, ArrayTypeName, Mapping]:
+        """
+        Returns:
+            Parent IR node.
+        """
         return self._parent
 
     @property
     def type(self) -> types.Mapping:
+        """
+        Returns:
+            Type description.
+        """
         t = super().type
         assert isinstance(t, types.Mapping)
         return t
 
     @property
-    def key_type(self) -> TypeNameAbc:
+    def key_type(self) -> Union[ElementaryTypeName, UserDefinedTypeName]:
+        """
+        Can only be:
+
+        - an [ElementaryTypeName][woke.ast.ir.type_name.elementary_type_name.ElementaryTypeName],
+        - a [UserDefinedTypeName][woke.ast.ir.type_name.user_defined_type_name.UserDefinedTypeName] of a [Contract][woke.ast.types.Contract] type,
+        - a [UserDefinedTypeName][woke.ast.ir.type_name.user_defined_type_name.UserDefinedTypeName] of an [Enum][woke.ast.types.Enum] type,
+        - a [UserDefinedTypeName][woke.ast.ir.type_name.user_defined_type_name.UserDefinedTypeName] of a [UserDefinedValueType][woke.ast.types.UserDefinedValueType] type.
+        Returns:
+            Mapping key type name.
+        """
         return self.__key_type
 
     @property
     def value_type(self) -> TypeNameAbc:
+        """
+        Returns:
+            Mapping value type name.
+        """
         return self.__value_type
