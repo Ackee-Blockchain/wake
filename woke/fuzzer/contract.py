3f588828e99421f632331d75c9593a7e0b4677b3
Mirek Škrabal <mirekskrabal94@gmail.com>
2022-09-12 09:43:29 +0100

    🐛 add fixes for generating fallback

--- woke/fuzzer/contract.py
+++ woke/fuzzer/contract.py
@@ -7,6 +7,7 @@ import web3.contract
 from eth_typing import HexStr
 from web3 import Web3
 from web3._utils.abi import get_abi_output_types
+from web3._utils.compat import TypedDict
 from web3.types import TxParams, RPCEndpoint
 from web3.method import Method
 
@@ -21,6 +22,16 @@ class NetworkKind(IntEnum):
     HARDHAT = 1,
     GANACHE = 2,
     GETH = 3
+
+
+class Abi:
+    @classmethod
+    def encode(self, arguments: Iterable, types: Iterable) -> HexStr:
+        return eth_abi.encode(types, arguments)
+
+    @classmethod
+    def decode(self, data: bytes, types: Iterable) -> Any:
+        return eth_abi.decode(types, data)
 
 
 #global interface for communicating with the devchain
@@ -40,6 +51,8 @@ class DevchainInterface:
             "debug_trace_transaction": Method(RPCEndpoint("debug_traceTransaction")),
             "anvil_enable_traces": Method(RPCEndpoint("anvil_enableTraces")),  # currently not implemented in anvil - ValueError: {'code': -32603, 'message': 'Not implemented'}
         })
+        self.__w3.eth.default_account = self.__w3.eth.accounts[0]
+        print(f"default acc: {self.__w3.eth.default_account}")
         client_version: str = self.__w3.clientVersion.lower()
         print(f"client version: {client_version}")
         if "anvil" in client_version:
@@ -55,7 +68,7 @@ class DevchainInterface:
     @property
     def dev_chain(self):
         return self.__dev_chain
-
+    
 
     def deploy(self, abi, bytecode, arguments: Iterable, params: Optional[TxParams] = None) -> web3.contract.Contract:
         factory = self.__w3.eth.contract(abi=abi, bytecode=bytecode)
@@ -68,27 +81,31 @@ class DevchainInterface:
         func = contract.get_function_by_selector(selector)(*arguments)
         return func.call(params)
 
+    
+    def fallback(self, contract, data: bytes, params: TxParams, return_tx: bool, request_type: RequestType) -> Any:
+        if not params:
+            params = {}
+        #if no from address is specified, use the default account
+        if not "from" in params:
+            params['from'] = self.__w3.eth.default_account
+        #set the to address to the value of contract on which the fallback function is called
+        params['to'] = contract.address
+        if data:
+            params['data'] =  data #eth_abi.encode(*arguments, *types) #eth_abi.encode(['uint256', 'address'], [666, contract.address]) #self.__w3.eth.default_account])
+        #TODO process the transaction inside the devhcain class and return
+        tx_hash = self.__w3.eth.send_transaction(params)
+        output = self.dev_chain.retrieve_transaction_data([], tx_hash, request_type)
+        print(f"type of output: {type(output)}")
+        return bytes.fromhex(output)
+
 
     def transact(self, contract: web3.contract.Contract, selector: HexStr, arguments: Iterable, params: TxParams, return_tx, request_type) -> Any:
         print("making a transaction")
         #start_time = time.time()
         func = contract.get_function_by_selector(selector)(*arguments)
         output_abi = get_abi_output_types(func.abi)
-        #contract.fallback().transact(params)
-        # priorities:
-        # 1. anvil_enableTraces
-        # 2. trace_transaction
-        # 3. call
-        # 4. debug_traceTransaction
-
-        #start = time.time()
         tx_hash = func.transact(params)
-        #print(f"transact: {time.time()-start}")
-        
-        #method = RequestKind.DEBUG_TRACE_TRANSACTION
-        #start = time.time()
         output = self.dev_chain.retrieve_transaction_data([], tx_hash, request_type)
-        #print(f"trace: {time.time()-start}")
         return eth_abi.abi.decode(output_abi, bytes.fromhex(output))  # type: ignore
 
 
@@ -121,12 +138,7 @@ class Contract:
 
     #TODO handle return data
     def fallback_handler(self, arguments: Iterable, params: TxParams, return_tx: bool, request_type: RequestType) -> Any:
-        print("making a fallback transaction")
-        print(self._contract.fallback.abi.get("inputs"))
-        print(self._contract.fallback.abi)
-        return self._contract.fallback().transact({'data': eth_abi.encode(['uint256'], [1])})# if not request_type == 'call' else self._contract_fallback(*arguments).call(params)
-        #return self._contract.fallback(eth_abi.encode(['uint256'], [1])).transact(params)# if not request_type == 'call' else self._contract_fallback(*arguments).call(params)
-        #return self._contract.fallback(*arguments).transact(params)# if not request_type == 'call' else self._contract_fallback(*arguments).call(params)
+        return dev_interface.fallback(self._contract, arguments[0] if arguments else None, params, return_tx, request_type)
 
 
     def call(self, selector: HexStr, arguments: Iterable, params: TxParams, return_tx: bool) -> Any:
