592b5488a7fab5cf74e406c55419226e8f44beb4
Jan K <aragahara@gmail.com>
2022-09-15 13:16:13 +0200

    ✨ Add min_times decorator to fuzzer

--- woke/testing/campaign.py
+++ woke/testing/campaign.py
@@ -49,56 +49,19 @@ class Campaign:
             flows, _ = self.__get_methods(seq, attr="flow")
             invs, _ = self.__get_methods(seq, attr="invariant")
 
-            called = Counter[str]()
             # point_coverage = Counter[str]()
 
-            for j in range(flows_count):
-                if flows:
+            try:
+                generated_flows = self._generate_flows(flows, flows_count, seq)
+                for f in flows:
+                    logger.info(f"{f[1]} {f[0].weight}: {generated_flows.count(f)}")
+            except ValueError as ex:
+                logger.exception("Exception caught while generating flows sequence")
+                raise ex
 
-                    def meets_preconditions(flow):
-                        flow = flow[0]
-                        meets_precondition = not hasattr(
-                            flow, "precondition"
-                        ) or flow.precondition(seq)
-                        meets_max_times = (
-                            not hasattr(flow, "max_times")
-                            or called[flow] < flow.max_times
-                        )
-                        return meets_precondition and meets_max_times
-
-                    flows_p, flows_not_p = partition(flows, meets_preconditions)
-                    if flows_not_p:
-                        logger.info(f"\nThe following flows' preconditions are falsy:")
-                        for flow_not_p in flows_not_p:
-                            logger.info(f"    {flow_not_p[1]}")
-                    else:
-                        logger.info(f"\n")
-
-                    if len(flows_p) == 0:
-                        logger.warning(
-                            "There are no rules satisfying conditions. Terminating the sequence..."
-                        )
-                        break
-
-                    flows_weights = []
-
-                    for flow in flows_p:
-                        if hasattr(flow[0], "weight"):
-                            flows_weights.append(flow[0].weight)
-                        else:
-                            flows_weights.append(100)
-
-                    logger.info("Weights:")
-                    for idx in range(len(flows_p)):
-                        logger.info(f"    {flows_p[idx][1]}: {flows_weights[idx]}")
-                    flow = random.choices(flows_p, weights=flows_weights, k=1)[0]
-                    called.update((flow[0],))  # type: ignore
-
-                    logger.info(
-                        f'\n{f"FLOW...":<9} {j:>4} IN SEQUENCE {i:>5} {flow[1]}:'
-                    )
-                    flow[0]()
-
+            for j, flow in enumerate(generated_flows):
+                logger.info(f'\n{f"FLOW...":<9} {j:>4} IN SEQUENCE {i:>5} {flow[1]}:')
+                flow[0]()
                 if not dry_run and invs:
                     for idx, inv in enumerate(invs):
                         logger.info(f'{"inv...":<33}{inv[1]}')
@@ -161,3 +124,65 @@ class Campaign:
         items.sort()
         for key, count in items:
             print(f"{key:.<80}{count:.>4}")
+
+    def _generate_flows(self, flows: Methods, flows_count: int, seq) -> Methods:
+        weights = []
+
+        min_times_sum = 0
+        for i, flow in enumerate(flows):
+            cf = flow[0]
+            if hasattr(cf, "min_times"):
+                min_times_sum += cf.min_times
+                if hasattr(cf, "max_times") and cf.min_times > cf.max_times:
+                    raise ValueError(
+                        f"Flow {flow[1]} has min_times greater than max_times"
+                    )
+
+            if hasattr(cf, "weight"):
+                weights.append(cf.weight)
+            else:
+                weights.append(100)
+        if min_times_sum > flows_count:
+            raise ValueError("Current min_times can't be satisfied")
+        generated_flows = []
+        generated_cnt = Counter[str]()
+
+        # weight needed for one flow call to be randomly generated, say a flow has
+        # weight == 3 * weight_unit => flow will be 3 times in generated_flows on average
+        weight_unit = sum(weights) / flows_count
+        for i, flow in enumerate(flows):
+            cf, fname = flow
+            if hasattr(cf, "min_times"):
+                generated_flows += [flow] * cf.min_times
+                generated_cnt[fname] += cf.min_times
+                # this removes weight from a flow that was generated because of min_times
+                weight = weights[i] - (weight_unit * cf.min_times)
+                weights[i] = weight if weight > 0 else 1
+
+        indexed_flows = {k: v for k, v in enumerate(flows)}
+        for _ in range(flows_count - len(generated_flows)):
+
+            def meets_preconditions(indexed_flow: Tuple[int, Tuple[Callable, str]]):
+                cf, fname = indexed_flow[1]
+                meets_precondition = not hasattr(cf, "precondition") or cf.precondition(
+                    seq
+                )
+                meets_max_times = (
+                    not hasattr(cf, "max_times") or generated_cnt[fname] < cf.max_times
+                )
+                return meets_precondition and meets_max_times
+
+            indexed_flows_p, _ = partition(indexed_flows.items(), meets_preconditions)
+            if len(indexed_flows_p) == 0:
+                raise ValueError("Conditions for flows could not be met")
+            indexed_flow = random.choices(
+                indexed_flows_p, weights=[weights[k] for k, v in indexed_flows_p], k=1
+            )[0]
+            generated_cnt.update((indexed_flow[1][1],))
+            generated_flows.append(indexed_flow[1])
+
+        random.shuffle(generated_flows)
+        logger.debug(
+            f"Generating following flow sequence {[flow[1] for flow in generated_flows]}"
+        )
+        return generated_flows
