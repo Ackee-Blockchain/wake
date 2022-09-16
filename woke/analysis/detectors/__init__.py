ac44341f8968653d241011bbbb0f74d15bb8c102
Michal Převrátil <michprev@gmail.com>
2022-09-16 21:15:28 +0100

    ✨ Add `call options not called` detectors

--- woke/analysis/detectors/__init__.py
+++ woke/analysis/detectors/__init__.py
@@ -1,4 +1,8 @@
 from .api import DetectorResult, detect, detector
+from .call_options_not_called import (
+    detect_function_call_options_not_called,
+    detect_old_gas_value_not_called,
+)
 from .reentrancy import detect_reentrancy
 from .unchecked_return_value import detect_unchecked_return_value
 from .unsafe_delegatecall import detect_unsafe_delegatecall
