d1538c2d538d996dcf5dcee7d96b1ceb273be387
Michal Převrátil <michprev@gmail.com>
2022-09-16 21:14:30 +0100

    ✨ Add `unsafe selfdestruct` detector

--- woke/analysis/detectors/__init__.py
+++ woke/analysis/detectors/__init__.py
@@ -2,3 +2,4 @@ from .api import DetectorResult, detect, detector
 from .reentrancy import detect_reentrancy
 from .unchecked_return_value import detect_unchecked_return_value
 from .unsafe_delegatecall import detect_unsafe_delegatecall
+from .unsafe_selfdestruct import detect_unsafe_selfdestruct
