870d5a77380347f5aacd9e567d749f97f8636511
Michal Převrátil <michprev@gmail.com>
2022-09-21 16:32:37 +0200

    ✨ Implement `get_package_version`

---
+++ woke/utils/version.py
@@ -0,0 +1,13 @@
+try:
+    from importlib import metadata
+except ImportError:
+    # Python < 3.8
+    import importlib_metadata as metadata
+
+
+def get_package_version(package_name: str) -> str:
+    """Get the version of the given package."""
+    try:
+        return metadata.version(package_name)
+    except metadata.PackageNotFoundError:
+        raise RuntimeError(f"Package {package_name} not found.")
