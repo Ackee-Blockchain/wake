172a0d5a9d608cb317136852638c4dbee6b5f875
Michal Převrátil <michprev@gmail.com>
2022-09-29 15:19:38 +0200

    🔖 Version 1.0.2

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Ackee-Blockchain/woke",
     author="Ackee Blockchain",
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(exclude=("tests",)),
     author_email="hello@ackeeblockchain.com",
     keywords=[
