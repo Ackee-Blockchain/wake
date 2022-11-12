3766a8008a27274d917031b693561dc443bffd6a
jaczkal <honza@pm.me>
2022-11-12 17:13:57 +0100

    ✨ Add examples for locked ether detector

---
+++ woke/analysis/examples/A.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: NONE
+
+pragma solidity ^0.8.13;
+
+contract ReceiveEther {
+    // Function to receive Ether. msg.data must be empty
+    receive() external payable {}
+
+    // Fallback function is called when msg.data is not empty
+    fallback() external payable {}
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+}
