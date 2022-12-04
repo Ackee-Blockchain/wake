9da123865593863f6430c3892385a764976c5c3d
Michal Převrátil <michprev@gmail.com>
2022-12-04 21:31:10 +0100

    ✨ Support JSON-RPC over IPC on unix

--- woke/testing/json_rpc/ipc.py
+++ woke/testing/json_rpc/ipc.py
@@ -1,15 +1,39 @@
+import platform
+
 from woke.testing.json_rpc.abc import ProtocolAbc
 
+if platform.system() == "Windows":
+    raise NotImplementedError("IPC communication is not supported yet on Windows")
 
-class IpcProtocol(ProtocolAbc):
-    def __init__(self, uri: str):
-        raise NotImplementedError("IPC communication is not supported yet")
 
-    def __enter__(self):
-        pass
+else:
+    import socket
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        pass
+    class IpcProtocol(ProtocolAbc):
+        _uri: str
+        _socket: socket.socket
 
-    def send_recv(self, data: str) -> str:
-        raise NotImplementedError("IPC communication is not supported yet")
+        def __init__(self, uri: str):
+            self._uri = uri
+
+        def __enter__(self):
+            self._socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+            self._socket.setblocking(False)
+            self._socket.connect(self._uri)
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            self._socket.close()
+
+        def send_recv(self, data: str) -> str:
+            self._socket.sendall(data.encode("utf-8"))
+            received = b""
+
+            while True:
+                try:
+                    received += self._socket.recv(4096)
+                except BlockingIOError:
+                    if not received.endswith((b"}", b"}\n", b"]", b"]\n")):
+                        continue
+                    break
+
+            return received.decode("utf-8")
