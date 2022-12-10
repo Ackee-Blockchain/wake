828b8190bd69b50c7c65b17ee016079e8cf1f879
Michal Převrátil <michprev@gmail.com>
2022-12-10 12:36:57 +0100

    ✨ Implement IPC communication on Windows

--- woke/testing/json_rpc/ipc.py
+++ woke/testing/json_rpc/ipc.py
@@ -4,8 +4,41 @@ import platform
 from woke.testing.json_rpc.abc import ProtocolAbc
 
 if platform.system() == "Windows":
-    raise NotImplementedError("IPC communication is not supported yet on Windows")
+    import win32file  # pyright: reportMissingImports=false
 
+    class IpcProtocol(ProtocolAbc):  # pyright: reportGeneralTypeIssues=false
+        _uri: str
+
+        def __init__(self, uri: str):
+            self._uri = uri
+
+        def __enter__(self):
+            self._handle = win32file.CreateFile(
+                self._uri,
+                win32file.GENERIC_READ | win32file.GENERIC_WRITE,
+                0,
+                None,
+                win32file.OPEN_EXISTING,
+                0,
+                None,
+            )
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            self._handle.close()
+
+        def send_recv(self, data: str):
+            win32file.WriteFile(self._handle, data.encode("utf-8"))
+            received = b""
+
+            while True:
+                res, data = win32file.ReadFile(self._handle, 4096)
+                received += data  # pyright: reportGeneralTypeIssues=false
+                if not received.rstrip().endswith((b"}", b"]")):
+                    continue
+                try:
+                    return json.loads(received.decode("utf-8"))
+                except json.JSONDecodeError:
+                    continue
 
 else:
     import socket
