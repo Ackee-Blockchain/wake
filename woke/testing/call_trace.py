3cbf76154b429550e98c1a7944d6202a19023af2
Michal Převrátil <michprev@gmail.com>
2023-01-08 22:01:20 +0100

    ✨ Add arguments to call traces

--- woke/testing/call_trace.py
+++ woke/testing/call_trace.py
@@ -2,8 +2,11 @@ from __future__ import annotations
 
 import enum
 import importlib
-from typing import Any, Dict, List, Optional
+import reprlib
+from typing import Any, Dict, List, Optional, Tuple, cast
 
+import eth_abi
+import eth_utils
 from rich.console import Console
 from rich.tree import Tree
 
@@ -19,6 +22,8 @@ from woke.testing.core import (
 from woke.testing.json_rpc.communicator import TxParams
 from woke.testing.utils import read_from_memory
 
+from . import hardhat_console
+
 
 class CallTraceKind(str, enum.Enum):
     CALL = "CALL"
@@ -33,6 +38,7 @@ class CallTraceKind(str, enum.Enum):
 class CallTrace:
     _contract_name: Optional[str]
     _function_name: str
+    _arguments: List
     _status: bool
     _value: int
     _kind: CallTraceKind
@@ -44,12 +50,14 @@ class CallTrace:
         self,
         contract_name: Optional[str],
         function_name: str,
+        arguments: List,
         value: int,
         kind: CallTraceKind,
         depth: int,
     ):
         self._contract_name = contract_name
         self._function_name = function_name
+        self._arguments = arguments
         self._value = value
         self._kind = kind
         self._depth = depth
@@ -77,6 +85,9 @@ class CallTrace:
         else:
             ret = self.function_name
 
+        if self.kind != CallTraceKind.INTERNAL:
+            ret += f"({', '.join([reprlib.repr(arg) for arg in self.arguments])})"
+
         ret += f" {'[green]✓[/green]' if self.status else '[red]✗[/red]'}"
 
         if self.kind != CallTraceKind.CALL:
@@ -93,6 +104,10 @@ class CallTrace:
     @property
     def function_name(self) -> str:
         return self._function_name
+
+    @property
+    def arguments(self) -> Tuple:
+        return tuple(self._arguments)
 
     @property
     def status(self) -> bool:
@@ -136,7 +151,12 @@ class CallTrace:
         if origin_fqn is None or origin_fqn not in contracts_by_fqn:
             assert "to" in tx_params
             root_trace = CallTrace(
-                f"Unknown({tx_params['to']})", "<???>", value, CallTraceKind.CALL, 1
+                f"Unknown({tx_params['to']})",
+                "<???>",
+                [b"" if "data" not in tx_params else tx_params["data"]],
+                value,
+                CallTraceKind.CALL,
+                1,
             )
         else:
             contract_name = origin_fqn.split(":")[-1]
@@ -152,7 +172,7 @@ class CallTrace:
                 and "receive" in contract_abi
             ):
                 root_trace = CallTrace(
-                    contract_name, "<receive>", value, CallTraceKind.CALL, 1
+                    contract_name, "<receive>", [], value, CallTraceKind.CALL, 1
                 )
             elif (
                 "data" not in tx_params
@@ -164,16 +184,38 @@ class CallTrace:
                     or contract_abi["fallback"]["stateMutability"] == "payable"
                 ):
                     root_trace = CallTrace(
-                        contract_name, "<fallback>", value, CallTraceKind.CALL, 1
+                        contract_name,
+                        "<fallback>",
+                        [b"" if "data" not in tx_params else tx_params["data"]],
+                        value,
+                        CallTraceKind.CALL,
+                        1,
                     )
                 else:
                     root_trace = CallTrace(
-                        contract_name, "<???>", value, CallTraceKind.CALL, 1
+                        contract_name,
+                        "<???>",
+                        [b"" if "data" not in tx_params else tx_params["data"]],
+                        value,
+                        CallTraceKind.CALL,
+                        1,
                     )
             else:
                 fn_abi = contract_abi[tx_params["data"][:4]]
+                output_types = [
+                    eth_utils.abi.collapse_if_tuple(cast(Dict[str, Any], arg))
+                    for arg in fn_abi["inputs"]
+                ]
+                decoded_data = list(
+                    eth_abi.abi.decode(output_types, tx_params["data"][4:])
+                )  # pyright: reportGeneralTypeIssues=false
                 root_trace = CallTrace(
-                    contract_name, fn_abi["name"], value, CallTraceKind.CALL, 1
+                    contract_name,
+                    fn_abi["name"],
+                    decoded_data,
+                    value,
+                    CallTraceKind.CALL,
+                    1,
                 )
 
         current_trace = root_trace
@@ -198,10 +240,18 @@ class CallTrace:
             if log["op"] in {"CALL", "CALLCODE", "DELEGATECALL", "STATICCALL"}:
                 if log["op"] in {"CALL", "CALLCODE"}:
                     value = int(log["stack"][-3], 16)
+                    args_offset = int(log["stack"][-4], 16)
+                    args_size = int(log["stack"][-5], 16)
                 elif log["op"] == "DELEGATECALL":
                     value = values[-1]
+                    args_offset = int(log["stack"][-3], 16)
+                    args_size = int(log["stack"][-4], 16)
                 else:
                     value = 0
+                    args_offset = int(log["stack"][-3], 16)
+                    args_size = int(log["stack"][-4], 16)
+
+                data = bytes(read_from_memory(args_offset, args_size, log["memory"]))
 
                 assert current_trace is not None
 
@@ -211,13 +261,33 @@ class CallTrace:
                     if Address(addr) == Address(
                         "0x000000000000000000636F6e736F6c652e6c6f67"
                     ):
+                        if data[:4] in hardhat_console.abis:
+                            fn_abi = hardhat_console.abis[data[:4]]
+                            output_types = [
+                                eth_utils.abi.collapse_if_tuple(
+                                    cast(Dict[str, Any], arg)
+                                )
+                                for arg in fn_abi
+                            ]
+                            arguments = list(
+                                eth_abi.abi.decode(output_types, data[4:])
+                            )  # pyright: reportGeneralTypeIssues=false
+                        else:
+                            arguments = [data]
+
                         call_trace = CallTrace(
-                            "console", "log", value, log["op"], current_trace.depth + 1
+                            "console",
+                            "log",
+                            arguments,
+                            value,
+                            log["op"],
+                            current_trace.depth + 1,
                         )
                     else:
                         call_trace = CallTrace(
                             f"Unknown({Address(addr)})",
                             "<???>",
+                            [data],
                             value,
                             log["op"],
                             current_trace.depth + 1,
@@ -230,40 +300,47 @@ class CallTrace:
                         obj = getattr(obj, attr)
                     contract_abi = obj._abi
 
-                    if log["op"] in {"CALL", "CALLCODE"}:
-                        args_offset = int(log["stack"][-4], 16)
-                        args_size = int(log["stack"][-5], 16)
-                    else:
-                        args_offset = int(log["stack"][-3], 16)
-                        args_size = int(log["stack"][-4], 16)
-
                     if args_size >= 4:
-                        selector = bytes(
-                            read_from_memory(args_offset, 4, log["memory"])
-                        )
+                        selector = data[:4]
                         if selector in contract_abi:
-                            fn_name = contract_abi[selector]["name"]
+                            fn_abi = contract_abi[selector]
+                            output_types = [
+                                eth_utils.abi.collapse_if_tuple(
+                                    cast(Dict[str, Any], arg)
+                                )
+                                for arg in fn_abi["inputs"]
+                            ]
+                            arguments = list(
+                                eth_abi.abi.decode(output_types, data[4:])
+                            )  # pyright: reportGeneralTypeIssues=false
+                            fn_name = fn_abi["name"]
                         elif "fallback" in contract_abi and (
                             value == 0
                             or contract_abi["fallback"]["stateMutability"] == "payable"
                         ):
                             fn_name = "<fallback>"
+                            arguments = [data]
                         else:
                             fn_name = "<???>"
+                            arguments = [data]
                     else:
                         if args_size == 0 and "receive" in contract_abi:
                             fn_name = "<receive>"
+                            arguments = []
                         elif "fallback" in contract_abi and (
                             value == 0
                             or contract_abi["fallback"]["stateMutability"] == "payable"
                         ):
                             fn_name = "<fallback>"
+                            arguments = [data]
                         else:
                             fn_name = "<???>"
+                            arguments = [data]
 
                     call_trace = CallTrace(
                         contract_name,
                         fn_name,
+                        arguments,
                         value,
                         log["op"],
                         current_trace.depth + 1,
@@ -300,6 +377,7 @@ class CallTrace:
                             jump_trace = CallTrace(
                                 contract_name,
                                 function_name,
+                                [],
                                 current_trace._value,
                                 CallTraceKind.INTERNAL,
                                 current_trace.depth,
