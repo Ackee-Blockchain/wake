77e8cf0cd7282ff7ad05e67c99aa2b024c4e114d
Michal Převrátil <michprev@gmail.com>
2023-01-13 11:40:00 +0100

    ✨ Add `must_revert` and `may_revert` context managers

--- woke/testing/__init__.py
+++ woke/testing/__init__.py
@@ -2,9 +2,12 @@ from .core import Abi, Account, Address, ChainInterface, Wei, default_chain
 from .internal import (
     Error,
     Panic,
+    PanicCodeEnum,
     TransactionRevertedError,
     UnknownEvent,
     UnknownTransactionRevertedError,
+    may_revert,
+    must_revert,
 )
 from .transactions import LegacyTransaction
 from .utils import connect, keccak256, snapshot_and_revert
