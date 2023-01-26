fb81a5d3c2b2b42be60d07b77ca937c830e939ec
Michal Převrátil <michprev@gmail.com>
2023-01-26 18:30:56 +0100

    🚚 Rename `unchecked-function-return-value` -> `unchecked-return-value`

--- woke/analysis/detectors/__init__.py
+++ woke/analysis/detectors/__init__.py
@@ -16,7 +16,7 @@ from .overflow_calldata_tuple_reencoding_bug import (
 )
 from .proxy_contract_selector_clashes import ProxyContractSelectorClashDetector
 from .reentrancy import ReentrancyDetector
-from .unchecked_return_value import UncheckedFunctionReturnValueDetector
+from .unchecked_return_value import UncheckedReturnValueDetector
 from .unsafe_delegatecall import UnsafeDelegatecallDetector
 from .unsafe_selfdestruct import UnsafeSelfdestructDetector
 from .unsafe_tx_origin import UnsafeTxOriginDetector
