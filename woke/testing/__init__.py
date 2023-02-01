c3b04cdda0ff7be1fa17e3692af95c4be7f3e63e
Michal Převrátil <michprev@gmail.com>
2023-02-01 11:33:26 +0100

    🚧 Import primitive types in `woke.testing`

--- woke/testing/__init__.py
+++ woke/testing/__init__.py
@@ -9,5 +9,6 @@ from .internal import (
     may_revert,
     must_revert,
 )
+from .primitive_types import *
 from .transactions import LegacyTransaction
 from .utils import connect, keccak256, snapshot_and_revert
