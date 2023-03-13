192743f62b9ed3dceaad1912f81028ac988fe662
Michal Převrátil <michprev@gmail.com>
2023-03-13 22:23:58 +0100

    ✨ Implement `woke run`

--- woke/cli/__main__.py
+++ woke/cli/__main__.py
@@ -18,6 +18,7 @@ from .detect import run_detect
 from .fuzz import run_fuzz
 from .init import run_init
 from .lsp import run_lsp
+from .run import run_run
 from .svm import run_svm
 from .test import run_test
 
@@ -73,6 +74,7 @@ main.add_command(run_detect)
 main.add_command(run_fuzz)
 main.add_command(run_init)
 main.add_command(run_lsp)
+main.add_command(run_run)
 main.add_command(run_svm)
 main.add_command(run_test)
 
