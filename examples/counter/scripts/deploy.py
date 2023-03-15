998a8b3498ce83119898301cce8ced516eb3759e
Michal Převrátil <michprev@gmail.com>
2023-03-15 11:10:49 +0100

    🍱 Add example deployment script

---
+++ examples/counter/scripts/deploy.py
@@ -0,0 +1,13 @@
+from woke.deployment import *
+from pytypes.contracts.Counter import Counter
+
+# Use any node provider (Infura, Alchemy, etc.) or a local Geth node
+NODE_URL = "YOUR_NODE_URL"
+
+
+@default_chain.connect(NODE_URL)
+def main():
+    default_chain.set_default_accounts(Account.from_alias("deployment"))
+
+    counter = Counter.deploy()
+    counter.setCount(10)
