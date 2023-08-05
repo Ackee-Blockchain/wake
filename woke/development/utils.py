3b76816d15b0f3539c87b2b707e8bcb052857d09
Michal Převrátil <michprev@gmail.com>
2023-08-05 23:07:42 +0200

    ✨ Add helper functions for getting storage layout

--- woke/development/utils.py
+++ woke/development/utils.py
@@ -1,20 +1,57 @@
 from __future__ import annotations
 
+import asyncio
+import functools
+import importlib
 import inspect
 import json
 import math
 from collections import namedtuple
 from functools import lru_cache
-from typing import Callable, Dict, Iterable, List, Optional, Tuple, TypeVar, Union
+from json import JSONDecodeError
+from pathlib import Path, PurePosixPath
+from typing import (
+    Callable,
+    Dict,
+    Iterable,
+    List,
+    Optional,
+    Sequence,
+    Set,
+    Tuple,
+    TypeVar,
+    Union,
+)
 from urllib.error import URLError
 from urllib.parse import urlparse
 from urllib.request import Request, urlopen
 
 from Crypto.Hash import keccak
 from eth_utils.abi import function_abi_to_4byte_selector
+from pydantic import ValidationError, parse_raw_as
 
+from ..compiler import SolcOutputSelectionEnum, SolidityCompiler
+from ..compiler.solc_frontend import (
+    SolcInput,
+    SolcInputSource,
+    SolcOutputErrorSeverityEnum,
+    SolcOutputStorageLayout,
+    SolcOutputStorageLayoutStorage,
+    SolcOutputStorageLayoutType,
+)
+from ..config import WokeConfig
+from ..core.solidity_version import SolidityVersion
+from ..svm import SolcVersionManager
 from ..utils import get_package_version
-from .core import Abi, Account, Address
+from .chain_interfaces import ChainInterfaceAbc
+from .core import (
+    Abi,
+    Account,
+    Address,
+    get_contracts_by_fqn,
+    get_fqn_from_address,
+    get_user_defined_value_types_index,
+)
 from .globals import get_config
 
 ChainExplorer = namedtuple("ChainExplorer", ["url", "api_url"])
@@ -277,3 +314,184 @@ def get_logic_contract(contract: Account) -> Account:
         return Account(Abi.decode(["address"], impl_addr_raw)[0], chain=contract.chain)
 
     return contract
+
+
+def _get_storage_layout(contract: Account) -> SolcOutputStorageLayout:
+    fqn = get_fqn_from_address(contract.address, "latest", contract.chain)
+    if fqn is None:
+        if contract.chain._fork is None:
+            raise ValueError("Contract not found")
+
+        forked_chain_interface = ChainInterfaceAbc.connect(
+            get_config(), contract.chain._fork
+        )
+        try:
+            forked_chain_id = forked_chain_interface.get_chain_id()
+        finally:
+            forked_chain_interface.close()
+
+        if forked_chain_id not in chain_explorer_urls:
+            raise ValueError(
+                f"Chain explorer URL not found for chain ID {forked_chain_id}"
+            )
+
+        try:
+            return _get_storage_layout_from_explorer(
+                str(contract.address), contract.chain.chain_id
+            )
+        except Exception as e:
+            raise ValueError("Could not get storage layout from chain explorer") from e
+    else:
+        contracts_by_fqn = get_contracts_by_fqn()
+        module_name, attrs = contracts_by_fqn[fqn]
+        obj = getattr(importlib.import_module(module_name), attrs[0])
+        for attr in attrs[1:]:
+            obj = getattr(obj, attr)
+
+        if not hasattr(obj, "_storage_layout"):
+            raise ValueError("Could not get storage layout from contract source code")
+
+        return SolcOutputStorageLayout.parse_obj(obj._storage_layout)
+
+
+@functools.lru_cache(maxsize=64)
+def _get_storage_layout_from_explorer(
+    addr: str, chain_id: int
+) -> SolcOutputStorageLayout:
+    loop = asyncio.get_event_loop()
+
+    u = urlparse(chain_explorer_urls[chain_id].url)
+    config = get_config()
+    api_key = config.api_keys.get(".".join(u.netloc.split(".")[:-1]), None)
+    if api_key is None:
+        raise ValueError(f"Contract not found and API key for {u.netloc} not provided")
+
+    url = (
+        chain_explorer_urls[chain_id].api_url
+        + f"?module=contract&action=getsourcecode&address={addr}&apikey={api_key}"
+    )
+    req = Request(
+        url,
+        headers={
+            "Content-Type": "application/json",
+            "User-Agent": f"woke/{get_package_version('woke')}",
+        },
+    )
+
+    with urlopen(req) as response:
+        parsed = json.loads(response.read().decode("utf-8"))
+
+    if parsed["status"] != "1":
+        raise ValueError(f"Request to {u.netloc} failed: {parsed['result']}")
+
+    if "Proxy" in parsed["result"][0] and parsed["result"][0]["Proxy"] == "1":
+        return _get_storage_layout_from_explorer(
+            parsed["result"][0]["Implementation"], chain_id
+        )
+
+    version: str = parsed["result"][0]["CompilerVersion"]
+    if version.startswith("vyper"):
+        raise ValueError("Cannot set balance of Vyper contract")
+
+    if version.startswith("v"):
+        version = version[1:]
+    parsed_version = SolidityVersion.fromstring(version)
+
+    if parsed_version < SolidityVersion(0, 5, 13):
+        # storageLayout is only available in 0.5.13 and above
+        raise ValueError(f"Solidity version {parsed_version} too low, must be >=0.5.13")
+
+    optimizations = bool(parsed["result"][0]["OptimizationUsed"])
+    runs = parsed["result"][0]["Runs"]
+
+    config_dict = {
+        "compiler": {
+            "solc": {
+                "target_version": str(parsed_version),
+                "optimizer": {
+                    "enabled": optimizations,
+                    "runs": runs,
+                },
+            }
+        }
+    }
+
+    svm = SolcVersionManager(config)
+    if not svm.installed(parsed_version):
+        loop.run_until_complete(svm.install(parsed_version))
+
+    code = parsed["result"][0]["SourceCode"]
+    try:
+        standard_input: SolcInput = SolcInput.parse_raw(code[1:-1])
+        if any(
+            PurePosixPath(filename).is_absolute()
+            for filename in standard_input.sources.keys()
+        ):
+            raise ValueError("Absolute paths not allowed")
+        if (
+            standard_input.settings is not None
+            and standard_input.settings.remappings is not None
+        ):
+            config_dict["compiler"]["solc"][
+                "remappings"
+            ] = standard_input.settings.remappings
+
+        if any(source.urls is not None for source in standard_input.sources.values()):
+            raise NotImplementedError("Compilation from URLs not supported")
+
+        sources = {
+            config.project_root_path / path: source.content
+            for path, source in standard_input.sources.items()
+        }
+    except ValidationError:
+        try:
+            s = parse_raw_as(Dict[str, SolcInputSource], code)
+            if any(PurePosixPath(filename).is_absolute() for filename in s.keys()):
+                raise ValueError("Absolute paths not allowed")
+
+            if any(source.urls is not None for source in s.values()):
+                raise NotImplementedError("Compilation from URLs not supported")
+
+            sources = {
+                config.project_root_path / path: source.content
+                for path, source in s.items()
+            }
+        except (ValidationError, JSONDecodeError):
+            sources = {config.project_root_path / "tmp.sol": code}
+
+    compilation_config = WokeConfig.fromdict(
+        config_dict,
+        project_root_path=config.project_root_path,
+    )
+    compiler = SolidityCompiler(compilation_config)
+
+    graph, _ = compiler.build_graph(
+        sources.keys(), sources, True  # pyright: ignore reportGeneralTypeIssues
+    )
+    compilation_units = compiler.build_compilation_units_maximize(graph)
+    compilation_units = compiler.merge_compilation_units(compilation_units, graph)
+    if len(compilation_units) != 1:
+        raise ValueError("More than one compilation unit")
+    solc_output = loop.run_until_complete(
+        compiler.compile_unit_raw(
+            compilation_units[0],
+            parsed_version,
+            compiler.create_build_settings([SolcOutputSelectionEnum.STORAGE_LAYOUT]),
+        )
+    )
+
+    if any(e.severity == SolcOutputErrorSeverityEnum.ERROR for e in solc_output.errors):
+        raise ValueError("Errors during compilation")
+
+    contract_name = parsed["result"][0]["ContractName"]
+    try:
+        info = next(
+            c[contract_name]
+            for c in solc_output.contracts.values()
+            if contract_name in c
+        )
+    except StopIteration:
+        raise ValueError("Contract not found in compilation output")
+
+    assert info.storage_layout is not None
+    return info.storage_layout
