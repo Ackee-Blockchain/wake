d814d4269de891b92e6ef35f9e510c295ecc76ac
Michal Převrátil <michprev@gmail.com>
2023-08-06 11:54:28 +0200

    ✨ Implement ERC20 mint and burn helpers

--- woke/testing/__init__.py
+++ woke/testing/__init__.py
@@ -18,11 +18,13 @@ from woke.development.transactions import (
     on_revert,
 )
 from woke.development.utils import (
+    burn_erc20,
     get_create2_address_from_code,
     get_create2_address_from_hash,
     get_create_address,
     get_logic_contract,
     keccak256,
+    mint_erc20,
     read_storage_variable,
     write_storage_variable,
 )
