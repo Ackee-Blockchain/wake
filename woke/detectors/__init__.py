c6cd7c98eab0efe243dd658f9154696b5c239391
Michal Převrátil <michprev@gmail.com>
2023-09-05 14:25:05 +0200

    🔥 Remove implemented detectors from project

--- woke/detectors/__init__.py
+++ woke/detectors/__init__.py
@@ -7,22 +7,3 @@ from .api import (
     Detector,
     DetectorResult,
 )
-from .balance_state_var import UnsafeAddressBalanceUseDetector
-from .bug_empty_byte_array_copy import BugEmptyByteArrayCopyDetector
-from .call_options_not_called import FunctionCallOptionsNotCalledDetector
-from .missing_return import MissingReturnDetector
-from .msg_value_nonpayable_function import MsgValueNonpayableFunctionDetector
-from .overflow_calldata_tuple_reencoding_bug import (
-    OverflowCalldataTupleReencodingBugDetector,
-)
-from .proxy_contract_selector_clashes import ProxyContractSelectorClashDetector
-from .reentrancy import ReentrancyDetector
-from .unchecked_return_value import UncheckedReturnValueDetector
-from .unsafe_delegatecall import UnsafeDelegatecallDetector
-from .unsafe_selfdestruct import UnsafeSelfdestructDetector
-from .unsafe_tx_origin import UnsafeTxOriginDetector
-from .unused_contract import UnusedContractDetector
-
-from .always_reverts import AlwaysRevertsDetector  # isort:skip
-
-from . import axelar  # isort:skip
