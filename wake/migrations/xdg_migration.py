64c108f7664591f418b5960235d91ca66588789c
Michal Převrátil <michprev@gmail.com>
2023-11-01 13:49:13 +0100

    🐛 Fix XDG migration

--- wake/migrations/xdg_migration.py
+++ wake/migrations/xdg_migration.py
@@ -1,5 +1,6 @@
 import os
 import platform
+import shutil
 from pathlib import Path
 
 from ..cli.console import console
@@ -52,23 +53,39 @@ def run_xdg_migration() -> None:
     global_data_path.mkdir(parents=True, exist_ok=True)
 
     if config_path.exists():
-        config_path.rename(global_config_path)
-        console.print(
-            f"[green]Moved config file from {config_path} to {global_config_path} ✅[/]"
-        )
+        try:
+            config_path.rename(global_config_path)
+            console.print(
+                f"[green]Moved config file from {config_path} to {global_config_path} ✅[/]"
+            )
+        except OSError:
+            console.print(
+                f"[red]Failed to move config file from {config_path} to {global_config_path} ❌[/]"
+            )
     if compilers_path.exists():
-        compilers_path.rename(global_data_path / "compilers")
-        console.print(
-            f"[green]Moved compilers directory from {compilers_path} to {global_data_path / 'compilers'} ✅[/]"
-        )
+        try:
+            compilers_path.rename(global_data_path / "compilers")
+            console.print(
+                f"[green]Moved compilers directory from {compilers_path} to {global_data_path / 'compilers'} ✅[/]"
+            )
+        except OSError:
+            console.print(
+                f"[red]Failed to move compilers directory from {compilers_path} to {global_data_path / 'compilers'} ❌[/]"
+            )
+
     if solc_versions_path.exists():
-        solc_versions_path.rename(global_data_path / ".woke_solc_version")
-        console.print(
-            f"[green]Moved target solc versions file from {solc_versions_path} to {global_data_path / '.woke_solc_version'} ✅[/]"
-        )
+        try:
+            solc_versions_path.rename(global_data_path / ".woke_solc_version")
+            console.print(
+                f"[green]Moved target solc versions file from {solc_versions_path} to {global_data_path / '.woke_solc_version'} ✅[/]"
+            )
+        except OSError:
+            console.print(
+                f"[red]Failed to move target solc versions file from {solc_versions_path} to {global_data_path / '.woke_solc_version'} ❌[/]"
+            )
 
     try:
-        old_path.rmdir()
+        shutil.rmtree(old_path)
         console.print(f"[green]Removed old config directory {old_path} ✅[/]")
     except OSError:
         console.print(f"[red]Unable to remove old config directory {old_path} ❌[/]")
