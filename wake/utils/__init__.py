35f66c5d69339b5f930314f32264d4689ba88b11
Michal Převrátil <michprev@gmail.com>
2023-11-13 21:34:03 +0100

    🧑‍💻 Import `is_relative_to` in `wake.utils`

--- wake/utils/__init__.py
+++ wake/utils/__init__.py
@@ -1,5 +1,6 @@
 from .context_managers import change_cwd, recursion_guard
 from .decorators import return_on_recursion
 from .enums import StrEnum
+from .file_utils import is_relative_to
 from .general import get_class_that_defined_method
 from .version import get_package_version
