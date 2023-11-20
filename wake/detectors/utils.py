7cd27832e04479977dc5d3789a141b913c891b92
Michal Převrátil <michprev@gmail.com>
2023-11-20 17:48:07 +0100

    🐛 Fix SARIF export ignored detections

--- wake/detectors/utils.py
+++ wake/detectors/utils.py
@@ -2,7 +2,7 @@ import os
 import sys
 from inspect import cleandoc
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 from rich_click import Command
 from sarif_om import (
@@ -30,7 +30,7 @@ from .api import DetectorImpact, DetectorResult
 
 
 def create_sarif_log(
-    detectors: List[Command], detections: Dict[str, List[DetectorResult]]
+    detectors: List[Command], detections: List[Tuple[str, DetectorResult]]
 ) -> SarifLog:
     from wake.cli.detect import run_detect
 
@@ -91,99 +91,98 @@ def create_sarif_log(
         workspace_root = Path(workspace_root).resolve()
 
     results = []
-    for detector_name, r in detections.items():
-        for result in r:
-            (
-                start_line,
-                start_col,
-            ) = result.detection.ir_node.source_unit.get_line_col_from_byte_offset(
-                result.detection.ir_node.byte_location[0]
-            )
-            (
-                end_line,
-                end_col,
-            ) = result.detection.ir_node.source_unit.get_line_col_from_byte_offset(
-                result.detection.ir_node.byte_location[1]
-            )
-            rule = ReportingDescriptorReference(
-                id=detector_name,
-                index=detector_index_mapping[detector_name],
+    for detector_name, result in detections:
+        (
+            start_line,
+            start_col,
+        ) = result.detection.ir_node.source_unit.get_line_col_from_byte_offset(
+            result.detection.ir_node.byte_location[0]
+        )
+        (
+            end_line,
+            end_col,
+        ) = result.detection.ir_node.source_unit.get_line_col_from_byte_offset(
+            result.detection.ir_node.byte_location[1]
+        )
+        rule = ReportingDescriptorReference(
+            id=detector_name,
+            index=detector_index_mapping[detector_name],
+        )
+
+        source = run_detect.loaded_from_plugins[detector_name]
+        if (
+            isinstance(source, str)
+            and source in distributions
+            and len(distributions[source]) == 1
+        ):
+            package_name = distributions[source][0]
+            rule.tool_component = ToolComponentReference(
+                name=package_name,
+                index=extensions_index_mapping[package_name],
             )
 
-            source = run_detect.loaded_from_plugins[detector_name]
-            if (
-                isinstance(source, str)
-                and source in distributions
-                and len(distributions[source]) == 1
-            ):
-                package_name = distributions[source][0]
-                rule.tool_component = ToolComponentReference(
-                    name=package_name,
-                    index=extensions_index_mapping[package_name],
+        related_locations = []
+        for i, subdetection in enumerate(result.detection.subdetections):
+            (
+                sub_start_line,
+                sub_start_col,
+            ) = subdetection.ir_node.source_unit.get_line_col_from_byte_offset(
+                subdetection.ir_node.byte_location[0]
+            )
+            (
+                sub_end_line,
+                sub_end_col,
+            ) = subdetection.ir_node.source_unit.get_line_col_from_byte_offset(
+                subdetection.ir_node.byte_location[1]
+            )
+            related_locations.append(
+                Location(
+                    id=i,
+                    physical_location=PhysicalLocation(
+                        artifact_location=ArtifactLocation(
+                            uri=f"{subdetection.ir_node.source_unit.file if workspace_root is None else subdetection.ir_node.source_unit.file.relative_to(workspace_root)}",
+                        ),
+                        region=Region(
+                            start_line=sub_start_line,
+                            start_column=sub_start_col,
+                            end_line=sub_end_line,
+                            end_column=sub_end_col,
+                        ),
+                    ),
+                    message={"text": subdetection.message},
                 )
+            )
 
-            related_locations = []
-            for i, subdetection in enumerate(result.detection.subdetections):
-                (
-                    sub_start_line,
-                    sub_start_col,
-                ) = subdetection.ir_node.source_unit.get_line_col_from_byte_offset(
-                    subdetection.ir_node.byte_location[0]
-                )
-                (
-                    sub_end_line,
-                    sub_end_col,
-                ) = subdetection.ir_node.source_unit.get_line_col_from_byte_offset(
-                    subdetection.ir_node.byte_location[1]
-                )
-                related_locations.append(
+        results.append(
+            Result(
+                rule=rule,
+                level=impact_to_level[result.impact],
+                message={
+                    "text": result.detection.message
+                    + "\n"
+                    + "".join(
+                        f"[{related_locations[i].message['text']}]({i})"
+                        for i in range(len(related_locations))
+                    )
+                },
+                locations=[
                     Location(
-                        id=i,
                         physical_location=PhysicalLocation(
                             artifact_location=ArtifactLocation(
-                                uri=f"{subdetection.ir_node.source_unit.file if workspace_root is None else subdetection.ir_node.source_unit.file.relative_to(workspace_root)}",
+                                uri=f"{result.detection.ir_node.source_unit.file if workspace_root is None else result.detection.ir_node.source_unit.file.relative_to(workspace_root)}",
                             ),
                             region=Region(
-                                start_line=sub_start_line,
-                                start_column=sub_start_col,
-                                end_line=sub_end_line,
-                                end_column=sub_end_col,
+                                start_line=start_line,
+                                start_column=start_col,
+                                end_line=end_line,
+                                end_column=end_col,
                             ),
-                        ),
-                        message={"text": subdetection.message},
+                        )
                     )
-                )
-
-            results.append(
-                Result(
-                    rule=rule,
-                    level=impact_to_level[result.impact],
-                    message={
-                        "text": result.detection.message
-                        + "\n"
-                        + "".join(
-                            f"[{related_locations[i].message['text']}]({i})"
-                            for i in range(len(related_locations))
-                        )
-                    },
-                    locations=[
-                        Location(
-                            physical_location=PhysicalLocation(
-                                artifact_location=ArtifactLocation(
-                                    uri=f"{result.detection.ir_node.source_unit.file if workspace_root is None else result.detection.ir_node.source_unit.file.relative_to(workspace_root)}",
-                                ),
-                                region=Region(
-                                    start_line=start_line,
-                                    start_column=start_col,
-                                    end_line=end_line,
-                                    end_column=end_col,
-                                ),
-                            )
-                        )
-                    ],
-                    related_locations=related_locations,
-                )
+                ],
+                related_locations=related_locations,
             )
+        )
 
     return SarifLog(
         schema_uri="https://json.schemastore.org/sarif-2.1.0.json",
