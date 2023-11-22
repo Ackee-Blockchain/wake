9b17466cc0d70868d6cd9f7f456c5230cbb1bb77
Michal Převrátil <michprev@gmail.com>
2023-11-22 01:09:00 +0100

    ✨ Add `inheritance-tree` printer

--- wake_printers/__init__.py
+++ wake_printers/__init__.py
@@ -1,4 +1,5 @@
 from .abi import AbiPrinter
 from .control_flow_graph import ControlFlowGraphPrinter
+from .inheritance_tree import InheritanceTreePrinter
 from .storage_layout import StorageLayoutPrinter
 from .tokens import TokensPrinter
---
+++ wake_printers/inheritance_tree.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+
+from typing import Tuple
+
+import networkx as nx
+import rich.tree
+import rich_click as click
+from rich import print
+
+import wake.ir as ir
+import wake.ir.types as types
+from wake.printers import Printer, printer
+
+
+class InheritanceTreePrinter(Printer):
+    _names: Tuple[str, ...]
+
+    def print(self) -> None:
+        pass
+
+    def visit_contract_definition(self, node: ir.ContractDefinition):
+        from rich.tree import Tree
+
+        def _generate(contract: ir.ContractDefinition, tree: rich.tree.Tree):
+            for inheritance_spec in contract.base_contracts:
+                base = inheritance_spec.base_name.referenced_declaration
+                assert isinstance(base, ir.ContractDefinition)
+                tree.add(f"[link={self.generate_link(base)}]{base.name}[/link]")
+                _generate(base, tree.children[-1])
+
+            return tree
+
+        if len(self._names) > 0 and node.name not in self._names:
+            return
+
+        tree = Tree(
+            f"[link={self.generate_link(node)}]{node.name}[/link] inheritance tree"
+        )
+        _generate(node, tree)
+        print(tree, "")
+
+    @printer.command(name="inheritance-tree")
+    @click.option(
+        "--name",
+        "-n",
+        "names",
+        type=str,
+        multiple=True,
+        help="Contract names",
+    )
+    def cli(self, names: Tuple[str, ...]) -> None:
+        """
+        Print inheritance tree of contracts.
+        """
+        self._names = names
