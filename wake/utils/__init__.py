1ae961f0d64a5ca21586a62d9cf4c977012917ae
Michal Převrátil <michprev@gmail.com>
2023-11-26 00:00:04 +0100

    ✨ Implement `cached_return_on_recursion` decorator

--- wake/utils/__init__.py
+++ wake/utils/__init__.py
@@ -1,5 +1,5 @@
 from .context_managers import change_cwd, recursion_guard
-from .decorators import return_on_recursion
+from .decorators import cached_return_on_recursion, return_on_recursion
 from .enums import StrEnum
 from .file_utils import is_relative_to
 from .general import get_class_that_defined_method
--- wake/utils/decorators.py
+++ wake/utils/decorators.py
@@ -1,17 +1,35 @@
-from functools import wraps
+from functools import lru_cache, wraps
 
 from .context_managers import recursion_guard
 
 
 def return_on_recursion(default):
-    arguments = set()
+    arguments_guard = set()
 
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            if (args, tuple(sorted(kwargs.items()))) in arguments:
+            if (args, tuple(sorted(kwargs.items()))) in arguments_guard:
                 return default
-            with recursion_guard(arguments, *args, **kwargs):
+            with recursion_guard(arguments_guard, *args, **kwargs):
+                return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+def cached_return_on_recursion(default, max_size=128):
+    arguments_guard = set()
+
+    def decorator(func):
+        func = lru_cache(max_size)(func)
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            if (args, tuple(sorted((kwargs.items())))) in arguments_guard:
+                return default
+            with recursion_guard(arguments_guard, *args, **kwargs):
                 return func(*args, **kwargs)
 
         return wrapper
