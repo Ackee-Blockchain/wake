0327a207ae566c7793459aadb80db405af85f445
jaczkal <honza@pm.me>
2023-12-05 14:55:53 +0000

    ✨ Add purge cache script for documentation

---
+++ purge_cache.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [[ -z "${DOMAIN_ZONE_ID}" ]]; then
+    echo -e "\033[0;31mERROR: Variable DOMAIN_ZONE_ID is missing, set it up in CI settings.\033[0m" >&2
+    exit 1
+fi
+
+if [[ -z "${DOMAIN_TOKEN}" ]]; then
+    echo -e "\033[0;31mERROR: Variable DOMAIN_KEY and DOMAIN_TOKEN are missing, set one of it in CI settings.\033[0m" >&2
+    exit 1
+fi
+
+SITEMAP_URL="https://ackeeblockchain.com/wake/docs/latest/sitemap.xml"
+PURGE_URLS=$(curl -s $SITEMAP_URL | grep -Eo '<loc>[^<]*' | cut -d '>' -f 2)
+
+for url in $PURGE_URLS
+do
+    echo "Purgin cache for ${url}"
+    RESPONSE=$(curl -X POST "https://api.cloudflare.com/client/v4/zones/${DOMAIN_ZONE_ID}/purge_cache" \
+         -H "Authorization: Bearer ${DOMAIN_TOKEN}" \
+         --data "{\"files\":[\"${url}\"]}")
+    SUCCESS=$(echo $RESPONSE | jq -r '.success')
+    if [[ "$SUCCESS" != "true" ]]; then
+        echo "Failed to purge $url. Response: $RESPONSE"
+        exit 1
+    fi
+done
