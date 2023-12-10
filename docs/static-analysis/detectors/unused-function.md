bf00f67848ec391772f0532da2adbae6d598c032
Michal Převrátil <michprev@gmail.com>
2023-12-10 23:31:41 +0000

    ✨ Add `unused-function` detector

---
+++ docs/static-analysis/detectors/unused-function.md
@@ -0,0 +1,18 @@
+# Unused function detector
+
+Reports private and internal functions that are not used in the source code.
+
+## Example
+
+```solidity hl_lines="2" linenums="1"
+contract C {
+    function _withdraw() private {
+        (bool success, ) = msg.sender.call{value: address(this).balance}("");
+        require(success, "Transfer failed.");
+    }
+}
+```
+
+## Parameters
+
+The detector does not accept any additional parameters.
---
+++ wake_detectors/unused_function.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+
+from typing import List
+
+import networkx as nx
+import rich_click as click
+
+import wake.ir as ir
+import wake.ir.types as types
+from wake.cli import SolidityName
+from wake.detectors import (
+    Detection,
+    Detector,
+    DetectorConfidence,
+    DetectorImpact,
+    DetectorResult,
+    detector,
+)
+from wake_detectors.utils import generate_detector_uri
+
+
+class UnusedFunctionDetector(Detector):
+    _detections: List[DetectorResult]
+
+    def __init__(self) -> None:
+        self._detections = []
+
+    def detect(self) -> List[DetectorResult]:
+        return self._detections
+
+    def visit_function_definition(self, node: ir.FunctionDefinition):
+        from wake.analysis.utils import get_all_base_and_child_declarations
+
+        if node.visibility in {
+            ir.enums.Visibility.PUBLIC,
+            ir.enums.Visibility.EXTERNAL,
+        } or node.kind not in {
+            ir.enums.FunctionKind.FUNCTION,
+            ir.enums.FunctionKind.FREE_FUNCTION,
+        }:
+            return
+
+        if all(
+            len(d.references) == 0 for d in get_all_base_and_child_declarations(node)
+        ):
+            self._detections.append(
+                DetectorResult(
+                    Detection(
+                        node,
+                        "Unused function",
+                    ),
+                    impact=DetectorImpact.INFO,
+                    confidence=DetectorConfidence.HIGH,
+                    uri=generate_detector_uri(
+                        name="unused-function",
+                        version=self.extra["package_versions"]["eth-wake"],
+                    ),
+                )
+            )
+
+    @detector.command(name="unused-function")
+    def cli(self) -> None:
+        pass
