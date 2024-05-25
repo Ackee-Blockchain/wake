b027dccf99d9ca68c1ad0d1b01c88b33f5303f10
Michal Převrátil <michprev@gmail.com>
2024-05-25 09:31:46 +0200

    ♻️ Hover Yul definitions as LSP printer

--- wake_printers/__init__.py
+++ wake_printers/__init__.py
@@ -5,6 +5,7 @@ from .imports_graph import ImportsGraphPrinter
 from .inheritance_graph import InheritanceGraphPrinter
 from .inheritance_tree import InheritanceTreePrinter
 from .lsp_openzeppelin_docs import LspOpenzeppelinDocsPrinter
+from .lsp_yul_definitions import LspYulDefinitionsPrinter
 from .modifiers import ModifiersPrinter
 from .state_changes import StateChangesPrinter
 from .storage_layout import StorageLayoutPrinter
