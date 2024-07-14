82130f20c3a3370059b1a3e24fab5e03a287f0a0
Michal Převrátil <michprev@gmail.com>
2024-07-14 12:02:03 +0200

    ✨ Implement cached workspace symbol LSP feature

--- wake/lsp/features/workspace_symbol.py
+++ wake/lsp/features/workspace_symbol.py
@@ -11,73 +11,172 @@ from wake.lsp.context import LspContext
 from wake.lsp.exceptions import LspError
 from wake.lsp.protocol_structures import ErrorCodes
 from wake.lsp.utils import declaration_to_symbol_kind, path_to_uri, uri_to_path
+from wake.lsp.utils.position import changes_to_byte_offset
 
 
 async def workspace_symbol(
     context: LspContext, params: WorkspaceSymbolParams
 ) -> List[WorkspaceSymbol]:
-    await context.compiler.output_ready.wait()
-
     query = params.query.lower()
-    symbols = []
+    declarations = []
 
-    for source_unit in context.compiler.source_units.values():
-        for decl in source_unit.declarations_iter():
-            if query not in decl.name.lower():
+    if not context.compiler.output_ready.is_set():
+        for path, source_unit in context.compiler.last_compilation_source_units.items():
+            if path not in context.compiler.early_opened_files and not path.exists():
                 continue
 
-            symbols.append(
-                (
-                    WorkspaceSymbol(
-                        name=decl.name,
-                        kind=declaration_to_symbol_kind(decl),
-                        container_name=decl.parent.name
-                        if isinstance(decl.parent, ContractDefinition)
-                        else None,
-                        location=WorkspaceSymbolUriLocation(
-                            uri=path_to_uri(source_unit.file)
-                        ),
-                    ),
-                    source_unit,
-                )
+            declarations.extend(
+                decl
+                for decl in source_unit.declarations_iter()
+                if query in decl.name.lower()
+            )
+    else:
+        await context.compiler.output_ready.wait()
+
+        for source_unit in context.compiler.source_units.values():
+            declarations.extend(
+                decl
+                for decl in source_unit.declarations_iter()
+                if query in decl.name.lower()
             )
 
-    symbols.sort(key=lambda x: (x[0].name, x[1].source_unit_name))
+        # also process files that could not be compiled in the last run but still exist
+        for path, source_unit in context.compiler.last_compilation_source_units.items():
+            if path not in context.compiler.source_units and (
+                path in context.compiler.early_opened_files or path.exists()
+            ):
+                declarations.extend(
+                    decl
+                    for decl in source_unit.declarations_iter()
+                    if query in decl.name.lower()
+                )
+
+    symbols = []
+    for decl in declarations:
+        symbols.append(
+            (
+                WorkspaceSymbol(
+                    name=decl.name,
+                    kind=declaration_to_symbol_kind(decl),
+                    container_name=decl.parent.name
+                    if isinstance(decl.parent, ContractDefinition)
+                    else None,
+                    location=WorkspaceSymbolUriLocation(
+                        uri=path_to_uri(decl.source_unit.file)
+                    ),
+                ),
+                decl.source_unit.source_unit_name,
+            )
+        )
+
+    symbols.sort(key=lambda x: (x[0].name, x[1]))
     return [x[0] for x in symbols]
 
 
 async def workspace_symbol_resolve(
     context: LspContext, symbol: WorkspaceSymbol
 ) -> WorkspaceSymbol:
-    await context.compiler.output_ready.wait()
-
     if not isinstance(symbol.location, WorkspaceSymbolUriLocation):
         return symbol
 
     path = uri_to_path(symbol.location.uri)
-    if path not in context.compiler.source_units:
-        raise LspError(ErrorCodes.InvalidParams, f"Unknown file {path}")
+    forward_changes = context.compiler.get_last_compilation_forward_changes(path, path)
 
-    found = False
-    for decl in context.compiler.source_units[path].declarations_iter():
-        if decl.name != symbol.name:
-            continue
+    if (
+        not context.compiler.output_ready.is_set()
+        and forward_changes is not None
+        and path in context.compiler.last_compilation_source_units
+    ):
+        if path not in context.compiler.last_compilation_source_units:
+            raise LspError(ErrorCodes.InvalidParams, f"Unknown file {path}")
 
-        parent_name = (
-            decl.parent.name if isinstance(decl.parent, ContractDefinition) else None
-        )
-        if parent_name != symbol.container_name:
-            continue
+        for decl in context.compiler.last_compilation_source_units[
+            path
+        ].declarations_iter():
+            if decl.name != symbol.name:
+                continue
 
-        found = True
-        symbol.location = Location(
-            uri=path_to_uri(path),
-            range=context.compiler.get_range_from_byte_offsets(
-                path, decl.byte_location
-            ),
-        )
+            parent_name = (
+                decl.parent.name
+                if isinstance(decl.parent, ContractDefinition)
+                else None
+            )
+            if parent_name != symbol.container_name:
+                continue
 
-    if not found:
-        raise LspError(ErrorCodes.InvalidParams, f"Unknown symbol {symbol.name}")
+            new_byte_start = (
+                changes_to_byte_offset(forward_changes[0 : decl.byte_location[0]])
+                + decl.byte_location[0]
+            )
+            new_byte_end = (
+                changes_to_byte_offset(forward_changes[0 : decl.byte_location[1]])
+                + decl.byte_location[1]
+            )
 
-    return symbol
+            symbol.location = Location(
+                uri=path_to_uri(path),
+                range=context.compiler.get_early_range_from_byte_offsets(
+                    path, (new_byte_start, new_byte_end)
+                ),
+            )
+            return symbol
+
+    await context.compiler.output_ready.wait()
+
+    if path in context.compiler.source_units:
+        for decl in context.compiler.source_units[path].declarations_iter():
+            if decl.name != symbol.name:
+                continue
+
+            parent_name = (
+                decl.parent.name
+                if isinstance(decl.parent, ContractDefinition)
+                else None
+            )
+            if parent_name != symbol.container_name:
+                continue
+
+            symbol.location = Location(
+                uri=path_to_uri(path),
+                range=context.compiler.get_range_from_byte_offsets(
+                    path, decl.byte_location
+                ),
+            )
+            return symbol
+    elif (
+        forward_changes is not None
+        and (path in context.compiler.early_opened_files or path.exists())
+        and path in context.compiler.last_compilation_source_units
+    ):
+        for decl in context.compiler.last_compilation_source_units[
+            path
+        ].declarations_iter():
+            if decl.name != symbol.name:
+                continue
+
+            parent_name = (
+                decl.parent.name
+                if isinstance(decl.parent, ContractDefinition)
+                else None
+            )
+            if parent_name != symbol.container_name:
+                continue
+
+            new_byte_start = (
+                changes_to_byte_offset(forward_changes[0 : decl.byte_location[0]])
+                + decl.byte_location[0]
+            )
+            new_byte_end = (
+                changes_to_byte_offset(forward_changes[0 : decl.byte_location[1]])
+                + decl.byte_location[1]
+            )
+
+            symbol.location = Location(
+                uri=path_to_uri(path),
+                range=context.compiler.get_early_range_from_byte_offsets(
+                    path, (new_byte_start, new_byte_end)
+                ),
+            )
+            return symbol
+
+    raise LspError(ErrorCodes.InvalidParams, f"Unknown symbol {symbol.name}")
