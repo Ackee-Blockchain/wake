912128fb0f8c16d4c8302083d1819b941de97059
Michal Převrátil <michprev@gmail.com>
2025-04-25 17:51:20 +0200

    🐛 Fix `IERC20Metadata.sol` compilation with older solc

--- contracts/wake/interfaces/IERC20Metadata.sol
+++ contracts/wake/interfaces/IERC20Metadata.sol
@@ -10,15 +10,15 @@ interface IERC20Metadata is IERC20 {
     /**
      * @dev Returns the name of the token.
      */
-    function name() external view returns (string memory);
+    function name() external view override returns (string memory);
 
     /**
      * @dev Returns the symbol of the token.
      */
-    function symbol() external view returns (string memory);
+    function symbol() external view override returns (string memory);
 
     /**
      * @dev Returns the decimals places of the token.
      */
-    function decimals() external view returns (uint8);
+    function decimals() external view override returns (uint8);
 }
