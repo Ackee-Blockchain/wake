e7d81b98d66915706bff17db53d9b26d755b65e8
Michal Převrátil <michprev@gmail.com>
2025-05-17 10:29:29 +0200

    🐛 Fix `ConnectContext` and `SnapshotRevertContext` debugger handling

--- wake/utils/connect_context.py
+++ wake/utils/connect_context.py
@@ -11,7 +11,17 @@ class ConnectContext:
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self.chain._disconnect()
+        from bdb import BdbQuit
+
+        from wake.development.globals import get_exception_handler
+
+        try:
+            if exc_type is not None and not isinstance(exc_value, BdbQuit):
+                exception_handler = get_exception_handler()
+                if exception_handler is not None:
+                    exception_handler(exc_type, exc_value, traceback)
+        finally:
+            self.chain._disconnect()
 
     def __call__(self, fn):
         def wrapper(*args, **kwargs):
--- wake/utils/snapshot_revert_context.py
+++ wake/utils/snapshot_revert_context.py
@@ -7,4 +7,14 @@ class SnapshotRevertContext:
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self.chain.revert(self.snapshot_id)
+        from bdb import BdbQuit
+
+        from wake.development.globals import get_exception_handler
+
+        try:
+            if exc_type is not None and not isinstance(exc_value, BdbQuit):
+                exception_handler = get_exception_handler()
+                if exception_handler is not None:
+                    exception_handler(exc_type, exc_value, traceback)
+        finally:
+            self.chain.revert(self.snapshot_id)
