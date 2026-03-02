e01f4b01d1ca0a5b010518bec080c263d2962115
Michal Převrátil <michprev@gmail.com>
2026-03-02 11:40:28 +0100

    ✨ Support Solidity 0.8.34

--- wake/config/wake_config.py
+++ wake/config/wake_config.py
@@ -536,7 +536,7 @@ class WakeConfig:
         Returns:
             Maximum supported Solidity version.
         """
-        return SolidityVersion.fromstring("0.8.33")
+        return SolidityVersion.fromstring("0.8.34")
 
     @property
     def detectors(self) -> DetectorsConfig:
